5cff31b6c9f18bec2b893f1a327c3027a7f0a8b4
overtrue <anzhengchao@gmail.com>
2015-01-26 14:38:49 +0800

    添加XML类

---
+++ src/Wechat/Util/XML.php
@@ -0,0 +1,14 @@
+<?php namespace Overtrue\Wechat\Util;
+
+class XML
+{
+    public function toArray($xml)
+    {
+        # code...
+    }
+
+    public function build()
+    {
+        # code...
+    }
+}
