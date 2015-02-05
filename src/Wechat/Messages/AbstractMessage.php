bead6a33b96bc58105121c14a11a3c576f577110
overtrue <anzhengchao@gmail.com>
2015-02-05 14:33:41 +0800

    删除重复的方法item

--- src/Wechat/Messages/AbstractMessage.php
+++ src/Wechat/Messages/AbstractMessage.php
@@ -116,38 +116,6 @@ abstract class AbstractMessage
     }
 
     /**
-     * 添加图文消息内容
-     *
-     * @return void
-     */
-    public function item()
-    {
-        $args    = func_get_args();
-        $argsLen = func_num_args();
-
-        if ($argsLen && $args[0] instanceof Closure) {
-            return $args($this);
-        }
-
-        if ($argsLen < 3) {
-            throw new InvalidArgumentException("item方法要求至少3个参数：标题，描述，图片");
-        }
-
-        list($title, $description, $image, $url) = $args;
-
-        $item = array(
-            'Title'       => $title,
-            'Description' => $description,
-            'PicUrl'      => $image,
-            'Url'         => $url,
-        );
-
-        !empty($this->attributes['items']) || $this->attributes['items'] = array();
-
-        array_push($this->attributes['items'], $item);
-    }
-
-    /**
      * 调用不存在的方法
      *
      * @param string $method
