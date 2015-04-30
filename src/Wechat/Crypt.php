3d0840f70d05fdbf90452943a4652958d726f9a6
overtrue <anzhengchao@gmail.com>
2015-04-30 10:09:17 +0800

    bugfix

--- src/Wechat/Crypt.php
+++ src/Wechat/Crypt.php
@@ -59,7 +59,7 @@ class Crypt
      */
     public function __construct($appId, $token, $encodingAESKey)
     {
-        if (extension_loaded('mcrypt')) {
+        if (!extension_loaded('mcrypt')) {
             throw new Exception('Mcrypt 拓展未安装或未启用');
         }
 
