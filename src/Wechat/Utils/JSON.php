175d6e0bbdf6d352d3875597367f34276b455e48
overtrue <anzhengchao@gmail.com>
2015-05-19 14:58:30 +0800

    Email.

--- src/Wechat/Utils/JSON.php
+++ src/Wechat/Utils/JSON.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author    James ZHANG <james@todoadd.com>
+ * @author    James ZHANG <james.cau@gmail.com>
  * @copyright 2015 overtrue <i@overtrue.me>
  * @link      https://thenorthmemory.github.io
  */
