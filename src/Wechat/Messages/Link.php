84c38dd61392c4b6c82fb10284a229b0df33d0e2
overtrue <anzhengchao@gmail.com>
2015-05-19 15:00:17 +0800

    CS

--- src/Wechat/Messages/Link.php
+++ src/Wechat/Messages/Link.php
@@ -1,8 +1,16 @@
 <?php
 /**
- * Part of Overtrue\Wechat
+ * Link.php
  *
- * @author overtrue <i@overtrue.me>
+ * Part of Overtrue\Wechat.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    overtrue <i@overtrue.me>
+ * @copyright 2015 overtrue <i@overtrue.me>
+ * @link      https://github.com/overtrue
+ * @link      http://overtrue.me
  */
 
 namespace Overtrue\Wechat\Messages;
