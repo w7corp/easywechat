d94226632ddd57201697f31a2ce74afd611602cf
overtrue <anzhengchao@gmail.com>
2015-05-20 17:47:29 +0800

    添加入口类

---
+++ src/Wechat/Wechat.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Wechat.php
+ *
+ * Part of Overtrue\Wechat.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    overtrue <i@overtrue.me>
+ * @copyright 2015 overtrue <i@overtrue.me>
+ * @link      https://github.com/overtrue
+ * @link      http://overtrue.me
+ */
+
+namespace Overtrue\Wechat;
+
+/**
+ * SDK 入口
+ */
+class Wechat
+{
+
+}
