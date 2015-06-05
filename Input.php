0ded417e228232d1648c1236e1f3ad52acb885ad
overtrue <anzhengchao@gmail.com>
2015-06-05 19:03:10 +0800

    移动文件到指定目录

---
+++ Input.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Input.php
+ *
+ * Part of Overtrue\Wechat.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    overtrue <i@overtrue.me>
+ * @copyright 2015 overtrue <i@overtrue.me>
+ * @link      https://github.com/overtrue
+ * @link      http://overtrue.me
+ */
+
+namespace Overtrue\Wechat;
+
+use Overtrue\Wechat\Utils\Bag;
+
+class Input extends Bag
+{
+
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        parent::__construct(array_merge($_GET, $_POST));
+    }
+}
