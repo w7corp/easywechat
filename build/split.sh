d0a699977d5ae56cfa5f71847585530843cf9d4c
overtrue <anzhengchao@gmail.com>
2015-06-05 18:42:43 +0800

    拆分

---
+++ build/split.sh
@@ -0,0 +1,19 @@
+git subsplit publish "
+    src/Wechat/Core:git@github.com:easywechat/core.git
+    src/Wechat/Support/:git@github.com:easywechat/support.git
+    src/Wechat/User/:git@github.com:easywechat/user.git
+    src/Wechat/Menu/:git@github.com:easywechat/menu.git
+    src/Wechat/Js/:git@github.com:easywechat/js.git
+    src/Wechat/Message/:git@github.com:easywechat/message.git
+    src/Wechat/Semantic/:git@github.com:easywechat/semantic.git
+    src/Wechat/Store/:git@github.com:easywechat/store.git
+    src/Wechat/Url/:git@github.com:easywechat/url.git
+    src/Wechat/Material/:git@github.com:easywechat/material.git
+    src/Wechat/Notice/:git@github.com:easywechat/notice.git
+    src/Wechat/Server/:git@github.com:easywechat/server.git
+    src/Wechat/Stats/:git@github.com:easywechat/stats.git
+    src/Wechat/Staff/:git@github.com:easywechat/staff.git
+    src/Wechat/QRCode/:git@github.com:easywechat/qrcode.git
+    src/Wechat/OAuth/:git@github.com:easywechat/oauth.git
+    src/Wechat/Payment/:git@github.com:easywechat/payment.git
+" --no-heads --update --no-tags
