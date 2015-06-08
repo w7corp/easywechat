c8aae9ef4e3878aaa03a05be23af8a773f3ff2b3
overtrue <anzhengchao@gmail.com>
2015-06-08 12:02:18 +0800

    更新autoload.php

--- autoload.php
+++ autoload.php
@@ -2,6 +2,6 @@
 
 spl_autoload_register(function ($class) {
     if (false !== stripos($class, 'Overtrue\Wechat')) {
-        require_once __DIR__.'/src/'.str_replace('\\', '/', substr($class, 8)).'.php';
+        require_once __DIR__.'/src/'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 8)).'.php';
     }
 });
