4186a8ae36337293fba083983f9c13c90d343008
安正超 <anzhengchao@gmail.com>
2015-08-04 16:20:52 +0800

    Update Cache.php

--- src/Wechat/Cache.php
+++ src/Wechat/Cache.php
@@ -67,7 +67,7 @@ class Cache
 
         $data = array(
                  'data'       => $value,
-                 'expired_at' => time() + $lifetime - 500, //XXX: 减去 100 秒更可靠的说
+                 'expired_at' => time() + $lifetime - 500, //XXX: 微信API是世界上最烂的，没有之一
                 );
 
         if (!file_put_contents($this->getCacheFile($key), serialize($data))) {
