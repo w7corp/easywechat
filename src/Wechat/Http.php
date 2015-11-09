f1861a543d8989cd579c8317fb299dfef11edb9a
overtrue <anzhengchao@gmail.com>
2015-11-09 13:36:52 +0800

    添加 AccessToken 超时重试机制

--- src/Wechat/Http.php
+++ src/Wechat/Http.php
@@ -114,7 +114,7 @@ class Http extends HttpClient
             }
 
             // access token 超时重试处理
-            if ($contents['errcode'] == '40001' && $retry--) {
+            if ($contents['errcode'] == '40001' && $retry-- > 0) {
                 // force refresh
                 $this->token->getToken(true);
 
