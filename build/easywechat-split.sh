85fa3d089e509db768a30935f655ba438e57f44a
overtrue <anzhengchao@gmail.com>
2015-12-30 12:15:15 +0800

    Add ignore.

--- build/easywechat-split.sh
+++ build/easywechat-split.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+#
+TAGS="develop"
 
 split()
 {
-    SUBDIR=$1
+    SUBDIR=./.split/$1
     SPLIT=$2
     HEADS=$3
 
-    mkdir -p ./.build/$SUBDIR;
+    mkdir -p $SUBDIR;
 
     pushd $SUBDIR;
 
@@ -20,7 +22,7 @@ split()
         git subsplit init git@github.com:overtrue/wechat.git
         git subsplit update
 
-        time git subsplit publish --heads="$HEAD" --no-tags "$SPLIT" --rebuild-tags
+        time git subsplit publish --heads="$HEAD" "$SPLIT" --tags=$TAGS
 
         popd
 
@@ -28,27 +30,27 @@ split()
 
     popd
 
-    rm -rf ./.build
+    rm -rf $SUBDIR
 }
 
-split cache         src/Cache:git@github.com:easywechat/cache.git "3.0" &
-split card          src/Card:git@github.com:easywechat/card.git "3.0" &
-split core          src/Core:git@github.com:easywechat/core.git "3.0" &
-split device        src/Device:git@github.com:easywechat/device.git "3.0" &
-split encryption    src/Encryption:git@github.com:easywechat/encryption.git "3.0" &
-split js            src/Js:git@github.com:easywechat/js.git "3.0" &
-split material      src/Material:git@github.com:easywechat/material.git "3.0" &
-split menu          src/Menu:git@github.com:easywechat/menu.git "3.0" &
-split message       src/Message:git@github.com:easywechat/message.git "3.0" &
-split notice        src/Notice:git@github.com:easywechat/notice.git "3.0" &
-split payment       src/Payment:git@github.com:easywechat/payment.git "3.0" &
-split qrcode        src/QRCode:git@github.com:easywechat/qrcode.git "3.0" &
-split semantic      src/Semantic:git@github.com:easywechat/semantic.git "3.0" &
-split server        src/Server:git@github.com:easywechat/server.git "3.0" &
-split staff         src/Staff:git@github.com:easywechat/staff.git "3.0" &
-split stats         src/Stats:git@github.com:easywechat/stats.git "3.0" &
-split store         src/Store:git@github.com:easywechat/store.git "3.0" &
-split poi           src/POI:git@github.com:easywechat/poi.git "3.0" &
-split support       src/Support:git@github.com:easywechat/support.git "3.0" &
-split url           src/Url:git@github.com:easywechat/url.git "3.0" &
-split user          src/User:git@github.com:easywechat/user.git "3.0" &
+split cache         src/Cache:git@github.com:easywechat/cache.git $TAGS
+split card          src/Card:git@github.com:easywechat/card.git $TAGS
+split core          src/Core:git@github.com:easywechat/core.git $TAGS
+split device        src/Device:git@github.com:easywechat/device.git $TAGS
+split encryption    src/Encryption:git@github.com:easywechat/encryption.git $TAGS
+split js            src/Js:git@github.com:easywechat/js.git $TAGS
+split material      src/Material:git@github.com:easywechat/material.git $TAGS
+split menu          src/Menu:git@github.com:easywechat/menu.git $TAGS
+split message       src/Message:git@github.com:easywechat/message.git $TAGS
+split notice        src/Notice:git@github.com:easywechat/notice.git $TAGS
+split payment       src/Payment:git@github.com:easywechat/payment.git $TAGS
+split qrcode        src/QRCode:git@github.com:easywechat/qrcode.git $TAGS
+split semantic      src/Semantic:git@github.com:easywechat/semantic.git $TAGS
+split server        src/Server:git@github.com:easywechat/server.git $TAGS
+split staff         src/Staff:git@github.com:easywechat/staff.git $TAGS
+split stats         src/Stats:git@github.com:easywechat/stats.git $TAGS
+split store         src/Store:git@github.com:easywechat/store.git $TAGS
+split poi           src/POI:git@github.com:easywechat/poi.git $TAGS
+split support       src/Support:git@github.com:easywechat/support.git $TAGS
+split url           src/Url:git@github.com:easywechat/url.git $TAGS
+split user          src/User:git@github.com:easywechat/user.git $TAGS
