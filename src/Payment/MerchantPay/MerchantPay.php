966c82898dca40532b02a09c76a4cccb1d9214ee
Alex <ac1982@users.noreply.github.com>
2016-02-29 22:00:53 +0800

    Added files via upload
    MerchantPay Class

---
+++ src/Payment/MerchantPay/MerchantPay.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * MerchantPay.php.
+ *
+ * @author    AC <alexever@gmail.com>
+ * @copyright 2015 overtrue <i@overtrue.me>
+ *
+ * @link      https://github.com/overtrue
+ * @link      http://overtrue.me
+ */
+namespace EasyWeChat\Payment\MerchantPay;
+
+use EasyWeChat\Payment\Merchant;
+
+/**
+ * Class MerchantPay.
+ */
+class MerchantPay
+{
+    /**
+     * @var API
+     */
+    protected $api;
+
+    /**
+     * Merchant instance.
+     *
+     * @var \EasyWeChat\Payment\Merchant
+     */
+    protected $merchant;
+
+    /**
+     * Constructor.
+     *
+     * @param Merchant $merchant
+     */
+    public function __construct(Merchant $merchant)
+    {
+        $this->merchant = $merchant;
+    }
+
+    /**
+     * Merchant setter.
+     *
+     * @param Merchant $merchant
+     */
+    public function setMerchant(Merchant $merchant)
+    {
+        $this->merchant = $merchant;
+    }
+
+    /**
+     * Merchant getter.
+     *
+     * @return Merchant
+     */
+    public function getMerchant()
+    {
+        return $this->merchant;
+    }
+
+    /**
+     * API setter.
+     *
+     * @param API $api
+     */
+    public function setAPI(API $api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * Return API instance.
+     *
+     * @return API
+     */
+    public function getAPI()
+    {
+        return $this->api ?: $this->api = new API($this->getMerchant());
+    }
+
+    /**
+     * Magic call.
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @return mixed
+     *
+     * @codeCoverageIgnore
+     */
+    public function __call($method, $args)
+    {
+        if (is_callable([$this->getAPI(), $method])) {
+            return call_user_func_array([$this->api, $method], $args);
+        }
+    }
+}
