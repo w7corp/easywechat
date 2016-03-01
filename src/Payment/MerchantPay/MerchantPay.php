496d73b1b11ac9f9f2020fba7502830ae26276d1
Alex <ac1982@users.noreply.github.com>
2016-03-01 09:27:37 +0800

    Added files via upload
    The Merchant Class formatted.

--- src/Payment/MerchantPay/MerchantPay.php
+++ src/Payment/MerchantPay/MerchantPay.php
@@ -25,84 +25,77 @@ use EasyWeChat\Payment\Merchant;
 /**
  * Class MerchantPay.
  */
-class MerchantPay
-{
-    /**
-     * @var API
-     */
-    protected $api;
+class MerchantPay {
+	/**
+	 * @var API
+	 */
+	protected $api;
 
-    /**
-     * Merchant instance.
-     *
-     * @var \EasyWeChat\Payment\Merchant
-     */
-    protected $merchant;
+	/**
+	 * Merchant instance.
+	 *
+	 * @var \EasyWeChat\Payment\Merchant
+	 */
+	protected $merchant;
 
-    /**
-     * Constructor.
-     *
-     * @param Merchant $merchant
-     */
-    public function __construct(Merchant $merchant)
-    {
-        $this->merchant = $merchant;
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @param Merchant $merchant
+	 */
+	public function __construct(Merchant $merchant) {
+		$this->merchant = $merchant;
+	}
 
-    /**
-     * Merchant setter.
-     *
-     * @param Merchant $merchant
-     */
-    public function setMerchant(Merchant $merchant)
-    {
-        $this->merchant = $merchant;
-    }
+	/**
+	 * Merchant setter.
+	 *
+	 * @param Merchant $merchant
+	 */
+	public function setMerchant(Merchant $merchant) {
+		$this->merchant = $merchant;
+	}
 
-    /**
-     * Merchant getter.
-     *
-     * @return Merchant
-     */
-    public function getMerchant()
-    {
-        return $this->merchant;
-    }
+	/**
+	 * Merchant getter.
+	 *
+	 * @return Merchant
+	 */
+	public function getMerchant() {
+		return $this->merchant;
+	}
 
-    /**
-     * API setter.
-     *
-     * @param API $api
-     */
-    public function setAPI(API $api)
-    {
-        $this->api = $api;
-    }
+	/**
+	 * API setter.
+	 *
+	 * @param API $api
+	 */
+	public function setAPI(API $api) {
+		$this->api = $api;
+	}
 
-    /**
-     * Return API instance.
-     *
-     * @return API
-     */
-    public function getAPI()
-    {
-        return $this->api ?: $this->api = new API($this->getMerchant());
-    }
+	/**
+	 * Return API instance.
+	 *
+	 * @return API
+	 */
+	public function getAPI() {
+		return $this->api ?: $this->api = new API($this->getMerchant());
+	}
 
-    /**
-     * Magic call.
-     *
-     * @param string $method
-     * @param array  $args
-     *
-     * @return mixed
-     *
-     * @codeCoverageIgnore
-     */
-    public function __call($method, $args)
-    {
-        if (is_callable([$this->getAPI(), $method])) {
-            return call_user_func_array([$this->api, $method], $args);
-        }
-    }
+	/**
+	 * Magic call.
+	 *
+	 * @param string $method
+	 * @param array  $args
+	 *
+	 * @return mixed
+	 *
+	 * @codeCoverageIgnore
+	 */
+	public function __call($method, $args) {
+		if (is_callable([$this->getAPI(), $method])) {
+			return call_user_func_array([$this->api, $method], $args);
+		}
+	}
 }
