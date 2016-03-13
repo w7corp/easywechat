581afded05426c0a49e097149a2fde1236010dc4
tianyong90 <412039588@qq.com>
2016-03-13 22:23:35 +0800

    code stle fix.

--- src/Payment/helpers.php
+++ src/Payment/helpers.php
@@ -18,6 +18,7 @@
  * @link      https://github.com/overtrue
  * @link      http://overtrue.me
  */
+
 namespace EasyWeChat\Payment;
 
 /**
@@ -43,22 +44,24 @@ function generate_sign(array $attributes, $key, $encryptMethod = 'md5')
  *
  * @return string
  */
-function get_client_ip() {
+function get_client_ip()
+{
     $ipAddress = '';
     if (getenv('HTTP_CLIENT_IP')) {
         $ipAddress = getenv('HTTP_CLIENT_IP');
-    } elseif(getenv('HTTP_X_FORWARDED_FOR')) {
+    } elseif (getenv('HTTP_X_FORWARDED_FOR')) {
         $ipAddress = getenv('HTTP_X_FORWARDED_FOR');
-    } elseif(getenv('HTTP_X_FORWARDED')) {
+    } elseif (getenv('HTTP_X_FORWARDED')) {
         $ipAddress = getenv('HTTP_X_FORWARDED');
-    } elseif(getenv('HTTP_FORWARDED_FOR')) {
+    } elseif (getenv('HTTP_FORWARDED_FOR')) {
         $ipAddress = getenv('HTTP_FORWARDED_FOR');
-    } elseif(getenv('HTTP_FORWARDED')) {
-       $ipAddress = getenv('HTTP_FORWARDED');
-    } elseif(getenv('REMOTE_ADDR')) {
+    } elseif (getenv('HTTP_FORWARDED')) {
+        $ipAddress = getenv('HTTP_FORWARDED');
+    } elseif (getenv('REMOTE_ADDR')) {
         $ipAddress = getenv('REMOTE_ADDR');
     } else {
         $ipAddress = 'UNKNOWN';
     }
+
     return $ipAddress;
 }
