f3a43289146e86dec68a3c98d20898b7ed90e3a1
overtrue <anzhengchao@gmail.com>
2016-03-25 22:31:15 +0800

    Bugfix #359

--- src/Payment/Payment.php
+++ src/Payment/Payment.php
@@ -164,17 +164,13 @@ class Payment
     /**
      * Generate js config for share user address.
      *
-     * @param string|accessToken $accessToken
-     * @param bool               $json
+     * @param string $accessToken
+     * @param bool   $json
      *
      * @return string|array
      */
     public function configForShareAddress($accessToken, $json = true)
     {
-        if ($accessToken instanceof AccessToken) {
-            $accessToken = $accessToken->getToken();
-        }
-
         $params = [
             'appId' => $this->merchant->app_id,
             'scope' => 'jsapi_address',
