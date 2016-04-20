4f050a107c175331973a7b631a755aac8c820d97
overtrue <anzhengchao@gmail.com>
2016-04-20 09:42:25 -0400

    Applied fixes from StyleCI

--- src/Foundation/ServiceProviders/StaffServiceProvider.php
+++ src/Foundation/ServiceProviders/StaffServiceProvider.php
@@ -19,7 +19,6 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-
 namespace EasyWeChat\Foundation\ServiceProviders;
 
 use EasyWeChat\Staff\Staff;
--- src/Payment/LuckyMoney/API.php
+++ src/Payment/LuckyMoney/API.php
@@ -18,7 +18,6 @@
  * @link      https://github.com/overtrue
  * @link      http://overtrue.me
  */
-
 namespace EasyWeChat\Payment\LuckyMoney;
 
 use EasyWeChat\Core\AbstractAPI;
