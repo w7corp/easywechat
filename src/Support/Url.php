db80e61ef2f740c8ca46a0ebebb009100da061a4
Ares <ares333@users.noreply.github.com>
2016-06-17 11:34:04 +0800

    NGINX HTTPS无法签名
    判断协议的地方有类型bug，vardump($_SERVER['SERVER_PORT'])在我这边是string类型。

--- src/Support/Url.php
+++ src/Support/Url.php
@@ -34,7 +34,7 @@ class Url
     {
         $protocol = (!empty($_SERVER['HTTPS'])
                         && $_SERVER['HTTPS'] !== 'off'
-                        || $_SERVER['SERVER_PORT'] === 443) ? 'https://' : 'http://';
+                        || (int)$_SERVER['SERVER_PORT'] === 443) ? 'https://' : 'http://';
 
         return $protocol.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     }
