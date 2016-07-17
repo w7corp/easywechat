6a3172aec7ce5dc081ae061b5ff8b71a394605e3
overtrue <anzhengchao@gmail.com>
2016-07-17 12:39:39 +0800

    CS.

--- src/Payment/Order.php
+++ src/Payment/Order.php
@@ -24,6 +24,24 @@ use EasyWeChat\Support\Attribute;
 
 /**
  * Class Order.
+ *
+ * @property string $body
+ * @property string $detail
+ * @property string $attach
+ * @property string $out_trade_no
+ * @property string $fee_type
+ * @property string $total_fee
+ * @property string $spbill_create
+ * @property string $time_start
+ * @property string $time_expire
+ * @property string $goods_tag
+ * @property string $notify_url
+ * @property string $trade_type
+ * @property string $product_id
+ * @property string $limit_pay
+ * @property string $openid
+ * @property string $sub_openid
+ * @property string $auth_code
  */
 class Order extends Attribute
 {
