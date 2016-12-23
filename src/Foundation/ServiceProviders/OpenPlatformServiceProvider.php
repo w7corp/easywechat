e965ce309ebbbe1d94a2b21001bf31c261145876
mingyoung <mingyoungcheung@gmail.com>
2016-12-23 12:44:12 +0800

    fix author.

--- src/Foundation/ServiceProviders/OpenPlatformServiceProvider.php
+++ src/Foundation/ServiceProviders/OpenPlatformServiceProvider.php
@@ -14,7 +14,7 @@
  *
  * This file is part of the wechat.
  *
- * (c) overtrue <i@overtrue.me>
+ * (c) mingyoung <mingyoungcheung@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
