ad9361ef530b5dd2a1954a779c291c9044b452ac
刘小乐 <scholer_l@live.com>
2017-01-04 11:12:35 +0800

    Update Card.php
    如果没有修改base_info，不需要向接口提交这个字段，如果提交了反而无法通过校验

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -498,7 +498,9 @@ class Card extends AbstractAPI
         $card[$type] = [];
 
         $cardInfo = [];
-        $cardInfo['base_info'] = $baseInfo;
+        if ($baseInfo) {
+            $cardInfo['base_info'] = $baseInfo;
+        }
 
         $card[$type] = array_merge($cardInfo, $especial);
 
