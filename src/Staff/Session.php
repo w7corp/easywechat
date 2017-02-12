f71af419e0ef16a25b4130ee064b4c20126f164f
overtrue <anzhengchao@gmail.com>
2017-02-12 05:52:15 +0000

    Apply fixes from StyleCI

--- src/Staff/Session.php
+++ src/Staff/Session.php
@@ -15,26 +15,24 @@
  * @author    overtrue <i@overtrue.me>
  * @copyright 2015 overtrue <i@overtrue.me>
  *
- * @link      https://github.com/overtrue
- * @link      http://overtrue.me
+ * @see      https://github.com/overtrue
+ * @see      http://overtrue.me
  */
+
 namespace EasyWeChat\Staff;
 
 use EasyWeChat\Core\AbstractAPI;
-use EasyWeChat\Support\Collection;
 
 /**
  * Class Session.
  */
 class Session extends AbstractAPI
 {
-
     const API_CREATE = 'https://api.weixin.qq.com/customservice/kfsession/create';
     const API_CLOSE = 'https://api.weixin.qq.com/customservice/kfsession/close';
     const API_GET = 'https://api.weixin.qq.com/customservice/kfsession/getsession';
     const API_LISTS = 'https://api.weixin.qq.com/customservice/kfsession/getsessionlist';
     const API_WAITERS = 'https://api.weixin.qq.com/customservice/kfsession/getwaitcase';
-
 
     /**
      * List all sessions of $account.
