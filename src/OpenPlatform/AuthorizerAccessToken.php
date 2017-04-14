5641c7c4d011a6d36a6f887c5bb16b943e0d09d3
mingyoung <mingyoungcheung@gmail.com>
2017-04-14 15:23:33 +0800

    variable name shortening.

--- src/OpenPlatform/AuthorizerAccessToken.php
+++ src/OpenPlatform/AuthorizerAccessToken.php
@@ -36,29 +36,28 @@ use EasyWeChat\Core\AccessToken as BaseAccessToken;
  *
  * AuthorizerAccessToken is responsible for the access token of the authorizer,
  * the complexity is that this access token also requires the refresh token
- * of the authorizer which is acquired by the open platform authorization
- * process.
+ * of the authorizer which is acquired by the open platform authorizer process.
  *
  * This completely overrides the original AccessToken.
  */
 class AuthorizerAccessToken extends BaseAccessToken
 {
     /**
-     * @var \EasyWeChat\OpenPlatform\Authorization
+     * @var \EasyWeChat\OpenPlatform\Authorizer
      */
-    protected $authorization;
+    protected $authorizer;
 
     /**
      * AuthorizerAccessToken constructor.
      *
-     * @param string                                 $appId
-     * @param \EasyWeChat\OpenPlatform\Authorization $authorization
+     * @param string                              $appId
+     * @param \EasyWeChat\OpenPlatform\Authorizer $authorizer
      */
-    public function __construct($appId, Authorization $authorization)
+    public function __construct($appId, Authorizer $authorizer)
     {
         parent::__construct($appId, null);
 
-        $this->authorization = $authorization;
+        $this->authorizer = $authorizer;
     }
 
     /**
@@ -70,7 +69,7 @@ class AuthorizerAccessToken extends BaseAccessToken
      */
     public function getToken($forceRefresh = false)
     {
-        $cached = $this->authorization->getAuthorizerAccessToken();
+        $cached = $this->authorizer->getAccessToken();
 
         if ($forceRefresh || empty($cached)) {
             return $this->refreshToken();
@@ -86,13 +85,13 @@ class AuthorizerAccessToken extends BaseAccessToken
      */
     protected function refreshToken()
     {
-        $token = $this->authorization->getApi()
+        $token = $this->authorizer->getApi()
             ->getAuthorizerToken(
-                $this->authorization->getAuthorizerAppId(),
-                $this->authorization->getAuthorizerRefreshToken()
+                $this->authorizer->getAppId(),
+                $this->authorizer->getRefreshToken()
             );
 
-        $this->authorization->setAuthorizerAccessToken($token['authorizer_access_token'], $token['expires_in'] - 1500);
+        $this->authorizer->setAccessToken($token['authorizer_access_token'], $token['expires_in'] - 1500);
 
         return $token['authorizer_access_token'];
     }
@@ -104,6 +103,6 @@ class AuthorizerAccessToken extends BaseAccessToken
      */
     public function getAppId()
     {
-        return $this->authorization->getAuthorizerAppId();
+        return $this->authorizer->getAppId();
     }
 }
--- tests/OpenPlatform/AuthorizerAccessTokenTest.php
+++ tests/OpenPlatform/AuthorizerAccessTokenTest.php
@@ -16,43 +16,33 @@ class AuthorizerAccessTokenTest extends TestCase
 {
     public function testGetToken()
     {
-        $appId = 'appid@123';
-        $cachedToken = 'token@123';
-        $auth = $this->make($appId, $cachedToken, null);
+        $auth = $this->make('appid@123', 'token@123', null);
 
-        $token = $auth->getToken();
-        $this->assertEquals($cachedToken, $token);
+        $this->assertEquals('token@123', $auth->getToken());
     }
 
     public function testGetTokenExpired()
     {
-        $appId = 'appid@123';
-        $newToken = 'token@456';
-        $auth = $this->make($appId, null, $newToken);
+        $auth = $this->make('appid@123', null, 'token@456');
 
-        $token = $auth->getToken();
-        $this->assertEquals($newToken, $token);
+        $this->assertEquals('token@456', $auth->getToken());
     }
 
     public function testGetTokenForced()
     {
-        $appId = 'appid@123';
-        $cachedToken = 'token@123';
-        $newToken = 'token@456';
-        $auth = $this->make($appId, $cachedToken, $newToken);
+        $auth = $this->make('appid@123', 'token@123', 'token@456');
 
-        $token = $auth->getToken(true);
-        $this->assertEquals($newToken, $token);
+        $this->assertEquals('token@456', $auth->getToken(true));
     }
 
     private function make($appId, $cachedToken, $newToken)
     {
-        /** @var Authorization|\Mockery\MockInterface $mock */
-        $mock = \Mockery::mock('EasyWeChat\OpenPlatform\Authorization');
-        $mock->shouldReceive('getAuthorizerAppId')->andReturn($appId);
-        $mock->shouldReceive('getAuthorizerRefreshToken')->andReturn($newToken);
-        $mock->shouldReceive('setAuthorizerAccessToken')->andReturn(true);
-        $mock->shouldReceive('getAuthorizerAccessToken')
+        /** @var \EasyWeChat\OpenPlatform\Authorizer|\Mockery\MockInterface $mock */
+        $mock = \Mockery::mock('EasyWeChat\OpenPlatform\Authorizer');
+        $mock->shouldReceive('getAppId')->andReturn($appId);
+        $mock->shouldReceive('getRefreshToken')->andReturn($newToken);
+        $mock->shouldReceive('setAccessToken')->andReturn(true);
+        $mock->shouldReceive('getAccessToken')
              ->andReturn($cachedToken);
         $mock->shouldReceive('getApi')
              ->andReturn(\Mockery::mock('EasyWeChat\OpenPlatform\Api\BaseApi', function ($mock) use ($newToken) {
