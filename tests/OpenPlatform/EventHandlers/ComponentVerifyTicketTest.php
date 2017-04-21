957020c4f5d380d566aa8208870a062a47b38c5c
overtrue <anzhengchao@gmail.com>
2017-04-21 02:21:02 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/OpenPlatform/EventHandlers/ComponentVerifyTicketTest.php
+++ tests/OpenPlatform/EventHandlers/ComponentVerifyTicketTest.php
@@ -1,9 +1,12 @@
 <?php
 
-/**
- * ComponentVerifyTicketTest.php.
+/*
+ * This file is part of the overtrue/wechat.
  *
- * @author mingyoung <mingyoungcheung@gmail.com>
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace EasyWeChat\Tests\OpenPlatform\EventHandlers;
