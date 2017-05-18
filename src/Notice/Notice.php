93743b7eccc68dab2c7f2b100fde6d354989560e
张铭阳 <mingyoungcheung@gmail.com>
2017-05-18 11:03:47 +0800

    Fix CS.

--- src/Notice/Notice.php
+++ src/Notice/Notice.php
@@ -81,21 +81,21 @@ class Notice extends AbstractAPI
 
         $this->messageBackup = $this->message;
     }
-    
+
     /**
-     * Set default color
+     * Set default color.
      *
      * @param string $color example: #0f0f0f
      *
-     * @return this
+     * @return $this
      */
     public function defaultColor($color)
     {
         $this->defaultColor = $color;
-        
+
         return $this;
     }
-    
+
     /**
      * Set industry.
      *
