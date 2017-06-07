7407df1600052112eb0ad617329d79c0d3901d8b
mingyoung <mingyoungcheung@gmail.com>
2017-06-07 11:06:17 +0800

    WeWork Attendance module.

---
+++ src/Applications/WeWork/Attendance/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\Applications\WeWork\Attendance;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}.
+     */
+    public function register(Container $container)
+    {
+        $container['we_work.attendance'] = function ($container) {
+            return new Client($container['we_work.access_token']);
+        };
+    }
+}
