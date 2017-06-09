02e890b0a7022d8c96ad92cfb2c868f3602767c1
overtrue <anzhengchao@gmail.com>
2017-06-09 13:49:32 +0800

    Add ServiceContainer.

---
+++ src/Support/ServiceContainer.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\Support;
+
+use Pimple\Container;
+
+/**
+ * Class ServiceContainer.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+class ServiceContainer extends Container
+{
+    /**
+     * @var array
+     */
+    protected $providers = [];
+
+    /**
+     * Add a provider.
+     *
+     * @param string $provider
+     *
+     * @return Factory
+     */
+    public function addProvider($provider)
+    {
+        array_push($this->providers, $provider);
+
+        return $this;
+    }
+
+    /**
+     * Set providers.
+     *
+     * @param array $providers
+     */
+    public function setProviders(array $providers)
+    {
+        $this->providers = [];
+
+        foreach ($providers as $provider) {
+            $this->addProvider($provider);
+        }
+    }
+
+    /**
+     * Return all providers.
+     *
+     * @return array
+     */
+    public function getProviders()
+    {
+        return $this->providers;
+    }
+
+    /**
+     * Register service providers.
+     */
+    protected function registerProviders()
+    {
+        foreach ($this->providers as $provider) {
+            $this->register(new $provider());
+        }
+    }
+
+    /**
+     * Magic get access.
+     *
+     * @param string $id
+     *
+     * @return mixed
+     */
+    public function __get($id)
+    {
+        return $this->offsetGet($id);
+    }
+
+    /**
+     * Magic set access.
+     *
+     * @param string $id
+     * @param mixed  $value
+     */
+    public function __set($id, $value)
+    {
+        $this->offsetSet($id, $value);
+    }
+}
