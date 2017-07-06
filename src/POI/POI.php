df9c3d03d6efa051a7d7b09ca788a7bf527368c7
overtrue <anzhengchao@gmail.com>
2017-07-06 10:34:00 +0800

    Resolve #774

--- src/POI/POI.php
+++ src/POI/POI.php
@@ -92,6 +92,16 @@ class POI extends AbstractAPI
     }
 
     /**
+     * @param array $data
+     *
+     * @return int
+     */
+    public function createAndGetId(array $data)
+    {
+        return $this->create($data)['poi_id'];
+    }
+
+    /**
      * Update a POI.
      *
      * @param int   $poiId
