196ed643dce5635002464857168fdd4ac78e8ea8
wanglong <master@wanglong.name>
2017-07-11 15:49:00 +0800

    临时二维码也是支持scene_str的，这里补充上

--- src/QRCode/QRCode.php
+++ src/QRCode/QRCode.php
@@ -32,6 +32,7 @@ class QRCode extends AbstractAPI
     const SCENE_MAX_VALUE = 100000;
     const SCENE_QR_CARD = 'QR_CARD';
     const SCENE_QR_TEMPORARY = 'QR_SCENE';
+    const SCENE_QR_TEMPORARY_STR = 'QR_STR_SCENE';
     const SCENE_QR_FOREVER = 'QR_LIMIT_SCENE';
     const SCENE_QR_FOREVER_STR = 'QR_LIMIT_STR_SCENE';
 
@@ -63,16 +64,24 @@ class QRCode extends AbstractAPI
     /**
      * Create temporary.
      *
-     * @param string $sceneId
+     * @param string $sceneValue
      * @param null   $expireSeconds
      *
      * @return \EasyWeChat\Support\Collection
      */
-    public function temporary($sceneId, $expireSeconds = null)
+    public function temporary($sceneValue, $expireSeconds = null)
     {
-        $scene = ['scene_id' => intval($sceneId)];
+        if (is_int($sceneValue) && $sceneValue > 0) {
+            $type = self::SCENE_QR_TEMPORARY;
+            $sceneKey = 'scene_id';
+        } else {
+            $type = self::SCENE_QR_TEMPORARY_STR;
+            $sceneKey = 'scene_str';
+        }
 
-        return $this->create(self::SCENE_QR_TEMPORARY, $scene, true, $expireSeconds);
+        $scene = [$sceneKey => $sceneValue];
+
+        return $this->create($type, $scene, true, $expireSeconds);
     }
 
     /**
