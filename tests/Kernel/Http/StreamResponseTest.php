a89e7ae3aec51a9cfe1615af3c09c082b2a622ac
安正超 <anzhengchao@gmail.com>
2017-08-02 13:41:55 +0800

    Apply fixes from StyleCI (#836)
    [ci skip] [skip ci]

--- tests/Kernel/Http/StreamResponseTest.php
+++ tests/Kernel/Http/StreamResponseTest.php
@@ -1,11 +1,19 @@
 <?php
 
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use EasyWeChat\Kernel\Http\StreamResponse;
 use EasyWeChat\Tests\TestCase;
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamDirectory;
 use org\bovigo\vfs\vfsStreamWrapper;
-
 
 class StreamResponseTest extends TestCase
 {
