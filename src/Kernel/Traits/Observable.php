3465159c4ee2013852e737036bce768d73e300a8
overtrue <anzhengchao@gmail.com>
2017-08-07 21:23:44 +0800

    Added Kernel\Traits\Observable.

---
+++ src/Kernel/Traits/Observable.php
@@ -0,0 +1,205 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\Kernel\Traits;
+
+use Closure;
+use EasyWeChat\Kernel\Contracts\EventHandlerInterface;
+use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
+use EasyWeChat\Kernel\ServiceContainer;
+
+/**
+ * Trait Observable.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+trait Observable
+{
+    /**
+     * @var array
+     */
+    protected $handlers = [];
+
+    /**
+     * @param \Closure|EventHandlerInterface|string $handler
+     * @param \Closure|EventHandlerInterface|string $condition
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     */
+    public function push($handler, $condition = '*')
+    {
+        list($handler, $condition) = $this->resolveHandlerAndCondition($handler, $condition);
+
+        if (!isset($this->handlers[$condition])) {
+            $this->handlers[$condition] = [];
+        }
+
+        array_push($this->handlers[$condition], $handler);
+    }
+
+    /**
+     * @param \Closure|EventHandlerInterface|string $handler
+     * @param \Closure|EventHandlerInterface|string $condition
+     */
+    public function unshift($handler, $condition = '*')
+    {
+        list($handler, $condition) = $this->resolveHandlerAndCondition($handler, $condition);
+
+        if (!isset($this->handlers[$condition])) {
+            $this->handlers[$condition] = [];
+        }
+
+        array_unshift($this->handlers[$condition], $handler);
+    }
+
+    /**
+     * @param string                                $condition
+     * @param \Closure|EventHandlerInterface|string $handler
+     */
+    public function observe($condition, $handler)
+    {
+        return $this->push($handler, $condition);
+    }
+
+    /**
+     * @param string                                $condition
+     * @param \Closure|EventHandlerInterface|string $handler
+     */
+    public function on($condition, $handler)
+    {
+        return $this->push($handler, $condition);
+    }
+
+    /**
+     * @param string|int $event
+     * @param array      ...$payload
+     *
+     * @return mixed|null
+     */
+    public function dispatch($event, $payload)
+    {
+        return $this->notify($event, $payload);
+    }
+
+    /**
+     * @param string|int $event
+     * @param array      ...$payload
+     *
+     * @return mixed|null
+     */
+    public function notify($event, $payload)
+    {
+        $result = null;
+
+        foreach ($this->handlers as $condition => $handlers) {
+            if ($condition === '*' || ($condition & $event) == $event) {
+                foreach ($handlers as $handler) {
+                    $response = $this->callHandler($handler, $payload);
+
+                    if (true === $response) {
+                        continue;
+                    }
+
+                    if (false === $response) {
+                        break;
+                    }
+
+                    if (!empty($response)) {
+                        $result = $response;
+                    }
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHandlers()
+    {
+        return $this->handlers;
+    }
+
+    /**
+     * @param \Closure $handler
+     * @param array    $payload
+     *
+     * @return mixed
+     */
+    protected function callHandler(Closure $handler, array $payload)
+    {
+        try {
+            return $handler($payload);
+        } catch (\Exception $e) {
+            if (isset($this->app) && $this->app instanceof ServiceContainer) {
+                $this->app['logger']->error($e->getCode().': '.$e->getMessage(), [
+                    'code' => $e->getCode(),
+                    'message' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ]);
+            }
+        }
+    }
+
+    /**
+     * @param $handler
+     *
+     * @return \Closure
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     */
+    protected function makeClosure($handler)
+    {
+        if (is_callable($handler)) {
+            return $handler;
+        }
+
+        if (is_string($handler)) {
+            if (!class_exists($handler)) {
+                throw new InvalidArgumentException(sprintf('Class "%s" not exists.', $handler));
+            }
+
+            if (!in_array(EventHandlerInterface::class, (new \ReflectionClass($handler))->getInterfaceNames())) {
+                throw new InvalidArgumentException(sprintf('Class "%s" not an instance of "%s".', $handler, EventHandlerInterface::class));
+            }
+
+            return function ($payload) use ($handler) {
+                return (new $handler($this->app ?? null))->handle($payload);
+            };
+        }
+
+        if ($handler instanceof EventHandlerInterface) {
+            return function () use ($handler) {
+                return $handler->handle(...func_get_args());
+            };
+        }
+
+        throw new InvalidArgumentException('No valid handler is found in arguments.');
+    }
+
+    /**
+     * @param $handler
+     * @param $condition
+     *
+     * @return array
+     */
+    protected function resolveHandlerAndCondition($handler, $condition): array
+    {
+        if (is_int($handler) || (is_string($handler) && !class_exists($handler))) {
+            list($handler, $condition) = [$condition, $handler];
+        }
+
+        return [$this->makeClosure($handler), $condition];
+    }
+}
