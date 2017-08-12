abb3d1f2b2839b41e9c9f12612aab0f4dbfc3f6c
overtrue <anzhengchao@gmail.com>
2017-08-12 13:58:44 +0800

    Fixed raw response when echostr in request.

---
+++ src/OfficialAccount/Server/Guard.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\OfficialAccount\Server;
+
+use EasyWeChat\Kernel\ServerGuard;
+
+/**
+ * Class Guard.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+class Guard extends ServerGuard
+{
+    /**
+     * @return bool
+     */
+    protected function shouldReturnRawResponse(): bool
+    {
+        return !is_null($this->app['request']->get('echostr'));
+    }
+}
