4e6ad669934c1b57d6e2b952eb4137d9dfe4723b
张铭阳 <mingyoungcheung@gmail.com>
2017-10-13 21:39:03 +0800

    MiniProgram template. (#959)

--- src/MiniProgram/TemplateMessage/Client.php
+++ src/MiniProgram/TemplateMessage/Client.php
@@ -38,4 +38,62 @@ class Client extends BaseClient
      * {@inheritdoc}.
      */
     protected $required = ['touser', 'template_id', 'form_id'];
+
+    /**
+     * @param int $offset
+     * @param int $count
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function list(int $offset, int $count)
+    {
+        return $this->httpPostJson('cgi-bin/wxopen/template/library/list', compact('offset', 'count'));
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function get(string $id)
+    {
+        return $this->httpPostJson('cgi-bin/wxopen/template/library/get', compact('id'));
+    }
+
+    /**
+     * @param string $id
+     * @param array  $keyword
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function add(string $id, array $keyword)
+    {
+        return $this->httpPostJson('cgi-bin/wxopen/template/add', [
+            'id' => $id,
+            'keyword_id_list' => $keyword,
+        ]);
+    }
+
+    /**
+     * @param string $templateId
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function delete(string $templateId)
+    {
+        return $this->httpPostJson('cgi-bin/wxopen/template/del', [
+            'template_id' => $templateId,
+        ]);
+    }
+
+    /**
+     * @param int $offset
+     * @param int $count
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function getTemplates(int $offset, int $count)
+    {
+        return $this->httpPostJson('cgi-bin/wxopen/template/list', compact('offset', 'count'));
+    }
 }
