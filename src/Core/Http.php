abb0733bee024331e7c3997307e56bcdf761ac52
overtrue <anzhengchao@gmail.com>
2017-11-11 16:18:04 +0800

    Fixed EasyWeChat/core#6

--- src/Core/Http.php
+++ src/Core/Http.php
@@ -264,7 +264,7 @@ class Http
     public function parseJSON($body)
     {
         if ($body instanceof ResponseInterface) {
-            $body = $body->getBody();
+            $body = $body->getBody()->getContents();
         }
 
         // XXX: json maybe contains special chars. So, let's FUCK the WeChat API developers ...
