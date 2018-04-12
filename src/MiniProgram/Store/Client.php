bb559dc4d33d31d26b765e9ded696a29a0521b05
bigface <saybye720@gmail.com>
2018-04-12 10:20:28 +0800

    微信门店小程序 (#1175)
    * 微信门店小程序
    
    Signed-off-by: bigface <saybye720@gmail.com>
    
    * fix style
    
    * fix style
    
    Signed-off-by: bigface <saybye720@gmail.com>
    
    * 添加测试用例
    
    fix 更新门店bug
    
    Signed-off-by: bigface <saybye720@gmail.com>
    
    * 完事测试用例，检查拼写错误
    
    Signed-off-by: bigface <saybye720@gmail.com>
    
    * 完善格式
    
    Signed-off-by: bigface <saybye720@gmail.com>
    
    * 门店小程序移动到MiniProgram
    
    Signed-off-by: bigface <saybye720@gmail.com>
    
    * 获取地区方法改为负数
    
    Signed-off-by: bigface <saybye720@gmail.com>

---
+++ src/MiniProgram/Store/Client.php
@@ -0,0 +1,175 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\MiniProgram\Store;
+
+use EasyWeChat\Kernel\BaseClient;
+
+/**
+ * Class Client.
+ *
+ * @author bigface <saybye720@gmail.com>
+ */
+class Client extends BaseClient
+{
+    /**
+     * Get WXA supported categories.
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function categories()
+    {
+        return $this->httpGet('wxa/get_merchant_category');
+    }
+
+    /**
+     * Get district from tencent map .
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function districts()
+    {
+        return $this->httpGet('wxa/get_district');
+    }
+
+    /**
+     * Search store from tencent map.
+     *
+     * @param int    $districtId
+     * @param string $keyword
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function searchFromMap(int $districtId, string $keyword)
+    {
+        $params = [
+            'districtid' => $districtId,
+            'keyword' => $keyword,
+        ];
+
+        return $this->httpPostJson('wxa/search_map_poi', $params);
+    }
+
+    /**
+     * Get store check status.
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function getStatus()
+    {
+        return $this->httpPostJson('wxa/get_merchant_audit_info');
+    }
+
+    /**
+     * Create a merchant.
+     *
+     * @param array $baseInfo
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function createMerchant(array $baseInfo)
+    {
+        return $this->httpPostJson('wxa/apply_merchant', $baseInfo);
+    }
+
+    /**
+     * Update a merchant.
+     *
+     * @param array $params
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function updateMerchant(array $params)
+    {
+        return $this->httpPostJson('wxa/modify_merchant', $params);
+    }
+
+    /**
+     * Create a store from tencent map.
+     *
+     * @param array $baseInfo
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function createFromMap(array $baseInfo)
+    {
+        return $this->httpPostJson('wxa/create_map_poi', $baseInfo);
+    }
+
+    /**
+     * Create a store.
+     *
+     * @param array $baseInfo
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function create(array $baseInfo)
+    {
+        return $this->httpPostJson('wxa/add_store', $baseInfo);
+    }
+
+    /**
+     * Update a store.
+     *
+     * @param int   $poiId
+     * @param array $baseInfo
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function update(int $poiId, array $baseInfo)
+    {
+        $params = array_merge($baseInfo, ['poi_id' => $poiId]);
+
+        return $this->httpPostJson('wxa/update_store', $params);
+    }
+
+    /**
+     * Get store by ID.
+     *
+     * @param int $poiId
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function get(int $poiId)
+    {
+        return $this->httpPostJson('wxa/get_store_info', ['poi_id' => $poiId]);
+    }
+
+    /**
+     * List store.
+     *
+     * @param int $offset
+     * @param int $limit
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function list(int $offset = 0, int $limit = 10)
+    {
+        $params = [
+            'begin' => $offset,
+            'limit' => $limit,
+        ];
+
+        return $this->httpPostJson('wxa/get_store_list', $params);
+    }
+
+    /**
+     * Delete a store.
+     *
+     * @param int $poiId
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function delete(int $poiId)
+    {
+        return $this->httpPostJson('wxa/del_store', ['poi_id' => $poiId]);
+    }
+}
