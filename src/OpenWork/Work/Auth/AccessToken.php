b60c9b1294b82b1ecc407a637ca5023b202e953a
xiaomin <keacefull@gmail.com>
2018-06-28 18:34:43 +0800

    加入企业微信服务商Api

--- src/OpenWork/Work/Auth/AccessToken.php
+++ src/OpenWork/Work/Auth/AccessToken.php
@@ -10,7 +10,7 @@
 
 namespace EasyWeChat\OpenWork\Work\Auth;
 
-use  EasyWeChat\Kernel\AccessToken as BaseAccessToken;
+use EasyWeChat\Kernel\AccessToken as BaseAccessToken;
 use EasyWeChat\OpenWork\Application;
 use Pimple\Container;
 
@@ -41,9 +41,9 @@ class AccessToken extends BaseAccessToken
 
     /**
      * AccessToken constructor.
-     * @param Container   $app
-     * @param String      $auth_corpid
-     * @param String      $permanent_code
+     * @param Container $app
+     * @param String $auth_corpid
+     * @param String $permanent_code
      * @param Application $component
      */
     public function __construct(Container $app, String $auth_corpid, String $permanent_code, Application $component)
