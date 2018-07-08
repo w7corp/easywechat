ac4b8fd907a13a8eef1c3fae94b6ae6e64535f79
安正超 <anzhengchao@gmail.com>
2018-07-08 10:39:12 +0800

    Apply fixes from StyleCI (#1292)
    [ci skip] [skip ci]

--- src/OpenPlatform/Authorizer/MiniProgram/Domain/Client.php
+++ src/OpenPlatform/Authorizer/MiniProgram/Domain/Client.php
@@ -24,6 +24,7 @@ class Client extends BaseClient
      * @param array $params
      *
      * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
      * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
      */
     public function modify(array $params)
@@ -38,6 +39,7 @@ class Client extends BaseClient
      * @param string $action
      *
      * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
      * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
      */
     public function setWebviewDomain(array $domains, $action = 'add')
