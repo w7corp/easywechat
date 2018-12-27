5ad40747d062783dd7b1bd8f2a535c9ce734cceb
张铭阳 <mingyoungcheung@gmail.com>
2018-12-27 12:32:34 +0800

    Add getPaidUnionid (#1442)
    * add getPaidUnionid
    
    * Apply fixes from StyleCI (#1441)
    
    [ci skip] [skip ci]

--- src/MiniProgram/Application.php
+++ src/MiniProgram/Application.php
@@ -52,5 +52,19 @@ class Application extends ServiceContainer
         BasicService\ContentSecurity\ServiceProvider::class,
         OpenData\ServiceProvider::class,
         Plugin\ServiceProvider::class,
+        Base\ServiceProvider::class,
     ];
+
+    /**
+     * Handle dynamic calls.
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        return $this->base->$method(...$args);
+    }
 }
---
+++ src/MiniProgram/Base/Client.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\MiniProgram\Base;
+
+use EasyWeChat\Kernel\BaseClient;
+
+/**
+ * Class Client.
+ *
+ * @author mingyoung <mingyoungcheung@gmail.com>
+ */
+class Client extends BaseClient
+{
+    /**
+     * Get paid unionid.
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     */
+    public function getPaidUnionid($openid, $options = [])
+    {
+        return $this->httpGet('wxa/getpaidunionid', compact('openid') + $options);
+    }
+}
