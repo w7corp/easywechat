862710634e1f90be413563fdf84ffe4e9539faea
ClouderSky <clouder.flow@gmail.com>
2019-01-22 12:08:46 +0800

    Add profit sharing client (#1457)
    * 通过sign_type参数选择签名算法
    
    * 实现分账相关接口
    
    * 修改方法命名
    
    * 修改服务组件命名
    
    * Update Client.php
    
    * Update BaseClient.php

--- src/Payment/Application.php
+++ src/Payment/Application.php
@@ -31,6 +31,7 @@ use EasyWeChat\OfficialAccount;
  * @property \EasyWeChat\BasicService\Url\Client          $url
  * @property \EasyWeChat\Payment\Transfer\Client          $transfer
  * @property \EasyWeChat\Payment\Security\Client          $security
+ * @property \EasyWeChat\Payment\ProfitSharing\Client     $profit_sharing
  * @property \EasyWeChat\OfficialAccount\Auth\AccessToken $access_token
  *
  * @method mixed pay(array $attributes)
@@ -56,6 +57,7 @@ class Application extends ServiceContainer
         Sandbox\ServiceProvider::class,
         Transfer\ServiceProvider::class,
         Security\ServiceProvider::class,
+        ProfitSharing\ServiceProvider::class,
     ];
 
     /**
