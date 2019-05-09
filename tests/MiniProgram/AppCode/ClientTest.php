edfeb2c02978a013464f2cd58f96de91a274c6d6
overtrue <anzhengchao@gmail.com>
2019-05-09 16:16:49 +0800

    Fixed tests.

--- tests/MiniProgram/AppCode/ClientTest.php
+++ tests/MiniProgram/AppCode/ClientTest.php
@@ -11,7 +11,9 @@
 
 namespace EasyWeChat\Tests\MiniProgram\AppCode;
 
+use EasyWeChat\Kernel\Http\Response;
 use EasyWeChat\Kernel\Http\StreamResponse;
+use EasyWeChat\Kernel\ServiceContainer;
 use EasyWeChat\MiniProgram\AppCode\Client;
 use EasyWeChat\Tests\TestCase;
 
@@ -23,7 +25,9 @@ class ClientTest extends TestCase
     {
         parent::setUp();
 
-        $this->mockStream = new \EasyWeChat\Kernel\Http\Response();
+        $this->mockStream = new \EasyWeChat\Kernel\Http\Response(200, [
+            'Content-disposition' => 'attachment',
+        ]);
     }
 
     public function testGetAppCode()
@@ -64,5 +68,20 @@ class ClientTest extends TestCase
         ]])->andReturn($this->mockStream);
 
         $this->assertInstanceOf(StreamResponse::class, $client->getQrCode('foo-path'));
+    }
+
+    public function testGetStreamWithNonStreamResponse()
+    {
+        $app = new ServiceContainer([
+            'response_type' => 'array',
+        ]);
+        $client = $this->mockApiClient(Client::class, [], $app);
+
+        $client->expects()->requestRaw('wxa/getwxacode', 'POST', ['json' => [
+            'path' => 'foo-path',
+            'width' => 430,
+        ]])->andReturn(new Response(200, [], '{"foo": "bar"}'));
+
+        $this->assertSame(['foo' => 'bar'], $client->get('foo-path', ['width' => 430]));
     }
 }
