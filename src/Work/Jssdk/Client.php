6f985d14fcef8433639a5ee4b10ea76a58d683a0
her-cat <i@her-cat.com>
2019-09-26 00:11:55 +0800

    添加企业微信获取应用的 jsapi_ticket 接口

--- src/Work/Jssdk/Client.php
+++ src/Work/Jssdk/Client.php
@@ -12,6 +12,7 @@
 namespace EasyWeChat\Work\Jssdk;
 
 use EasyWeChat\BasicService\Jssdk\Client as BaseClient;
+use EasyWeChat\Kernel\Exceptions\RuntimeException;
 
 /**
  * Class Client.
@@ -28,5 +29,40 @@ class Client extends BaseClient
     protected function getAppId()
     {
         return $this->app['config']->get('corp_id');
+    }
+
+    /**
+     * @param bool   $refresh
+     * @param string $type
+     *
+     * @return array|\EasyWeChat\Kernel\Support\Collection|mixed|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws RuntimeException
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function getAgentTicket(bool $refresh = false, string $type = 'agent_config')
+    {
+        $cacheKey = sprintf('easywechat.work.jssdk.ticket.%s.%s', $type, $this->getAppId());
+
+        if (!$refresh && $this->getCache()->has($cacheKey)) {
+            return $this->getCache()->get($cacheKey);
+        }
+
+        /** @var array<string, mixed> $result */
+        $result = $this->castResponseToType(
+            $this->requestRaw('cgi-bin/ticket/get', 'GET', ['query' => ['type' => $type]]),
+            'array'
+        );
+
+        $this->getCache()->set($cacheKey, $result, $result['expires_in'] - 500);
+
+        if (!$this->getCache()->has($cacheKey)) {
+            throw new RuntimeException('Failed to cache jssdk ticket.');
+        }
+
+        return $result;
     }
 }
