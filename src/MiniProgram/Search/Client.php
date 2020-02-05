ae1a7ae1d6093a22602d871d0baed980bd5cb0ab
her-cat <i@her-cat.com>
2020-02-06 00:56:35 +0800

    提交小程序页面支持

---
+++ src/MiniProgram/Search/Client.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\MiniProgram\Search;
+
+use EasyWeChat\Kernel\BaseClient;
+
+/**
+ * Class Client.
+ *
+ * @author her-cat <i@her-cat.com>
+ */
+class Client extends BaseClient
+{
+    /**
+     * Submit applet page URL and parameter information.
+     *
+     * @param array $pages
+     *
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function submitPage(array $pages)
+    {
+        return $this->httpPostJson('wxa/search/wxaapi_submitpages', compact('pages'));
+    }
+}
