124c8de9349d1d5e04dff9f5b5d308b916716dd3
Harry <L3478830@163.com>
2020-04-07 14:22:33 +0800

    增加获取获取打印员接口

--- src/MiniProgram/Express/Client.php
+++ src/MiniProgram/Express/Client.php
@@ -99,6 +99,18 @@ class Client extends BaseClient
         ]);
     }
 
+
+    /**
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getPrinter()
+    {
+        return $this->httpPostJson('cgi-bin/express/business/printer/getall');
+    }
+
+
     /**
      * @param string $openid
      *
