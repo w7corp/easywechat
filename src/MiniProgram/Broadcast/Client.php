674458a1589506417001791e103d1b82ca318ac6
Abbotton <uctoo@foxmial.com>
2020-05-25 12:04:11 +0800

    新增获取商品列表接口和向直播间导入商品接口

--- src/MiniProgram/Broadcast/Client.php
+++ src/MiniProgram/Broadcast/Client.php
@@ -135,4 +135,30 @@ class Client extends BaseClient
 
         return $this->httpGet('wxa/business/getgoodswarehouse', $params);
     }
+    
+    /**
+     * Get goods list based on status
+     *
+     * @param  array  $params
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getApproved(array $params)
+    {
+        return $this->httpGet('wxaapi/broadcast/goods/getapproved', $params);
+    }
+    
+    /**
+     * Add goods to the designated live room.
+     *
+     * @param  array  $params
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function addGoods(array $params)
+    {
+        return $this->httpPost('wxaapi/broadcast/room/addgoods', $params);
+    }
 }
