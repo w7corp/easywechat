6de3e9cf99f3b0d6c3f82e0f36553df32e5363b4
Abbotton <uctoo@foxmial.com>
2020-05-28 11:20:46 +0800

    增加创建直播间接口

--- src/MiniProgram/Broadcast/Client.php
+++ src/MiniProgram/Broadcast/Client.php
@@ -161,4 +161,17 @@ class Client extends BaseClient
     {
         return $this->httpPost('wxaapi/broadcast/room/addgoods', $params);
     }
+    
+    /**
+     * Create a live room.
+     *
+     * @param  array  $params
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function createLiveRoom(array $params)
+    {
+        return $this->httpPost('wxaapi/broadcast/room/create', $params);
+    }
 }
