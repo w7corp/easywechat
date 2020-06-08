10fe3906c4c1ac4d3bebe28e83a5120bad66947c
fideism <nignog@126.com>
2020-06-08 19:55:14 +0800

    企业微信小程序消息

---
+++ src/Kernel/Messages/MiniprogramNotice.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace EasyWeChat\Kernel\Messages;
+
+
+class MiniprogramNotice extends Message
+{
+    protected $type = 'miniprogram_notice';
+
+    protected $properties = [
+        'appid',
+        'title',
+    ];
+}
