0eb0273634dbc3b6e2867bbe18c3c7b18d1366ec
noikiy <noikiy@live.com>
2020-09-12 14:47:05 +0800

    第三方代授权小程序增加获取永久素材接口

--- src/OpenPlatform/Authorizer/MiniProgram/Application.php
+++ src/OpenPlatform/Authorizer/MiniProgram/Application.php
@@ -19,11 +19,12 @@ use EasyWeChat\OpenPlatform\Authorizer\Aggregate\AggregateServiceProvider;
  *
  * @author mingyoung <mingyoungcheung@gmail.com>
  *
- * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Account\Client $account
- * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Code\Client    $code
- * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Domain\Client  $domain
- * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Setting\Client $setting
- * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Tester\Client  $tester
+ * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Account\Client  $account
+ * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Code\Client     $code
+ * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Domain\Client   $domain
+ * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Setting\Client  $setting
+ * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Tester\Client   $tester
+ * @property \EasyWeChat\OpenPlatform\Authorizer\MiniProgram\Material\Client $material
  */
 class Application extends MiniProgram
 {
@@ -44,6 +45,7 @@ class Application extends MiniProgram
             Account\ServiceProvider::class,
             Setting\ServiceProvider::class,
             Tester\ServiceProvider::class,
+            Material\ServiceProvider::class,
         ];
 
         foreach ($providers as $provider) {
