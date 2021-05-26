94e296cbc4fee7bceef0202f52fa2d5579e3e917
项英杰 <506907958@qq.com>
2021-05-26 10:14:08 +0800

    添加 专门用来回复任务卡片消息（InteractiveTaskCard）的类型     此类型无法直接发送使用，只能通过企业微信应用消息API接受到任务卡片消息时回复时使用

---
+++ src/Kernel/Messages/ReplyInteractiveTaskCard.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\Kernel\Messages;
+
+/**
+ * Class ReplyInteractiveTaskCard
+ *
+ * @property string replace_name
+ *
+ * @description 专门为回复 InteractiveTaskCard 类型任务卡片消息而创建的类型
+ * @author      xyj2156
+ *
+ * @package     App\Extend\EnterpriseApplication\BusinessWX\Message
+ */
+class ReplyInteractiveTaskCard extends Message
+{
+    /**
+     * Message Type
+     *
+     * @var string
+     */
+    protected $type = 'update_taskcard';
+
+    /**
+     * Properties.
+     *
+     * @var array
+     */
+    protected $properties = [
+        'replace_name',
+    ];
+
+    /**
+     * ReplyInteractiveTaskCard constructor.
+     *
+     * @param string $replace_name
+     */
+    public function __construct(string $replace_name = '')
+    {
+        parent::__construct(compact('replace_name'));
+    }
+
+    public function toXmlArray()
+    {
+        return [
+            'TaskCard' => [
+                'ReplaceName' => $this->get('replace_name'),
+            ],
+        ];
+    }
+}
