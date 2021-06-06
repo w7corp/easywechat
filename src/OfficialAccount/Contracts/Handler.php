ff8f9ead6a602ba72b384c9af510f6d9ce8a0f50
刘豪 <liuhao25@foxmail.com>
2021-06-06 21:39:14 +0800

    [refactor]official-account contracts refactor (#2102)
    * [refactor]Observable 调整
    
    * [refactor]official-account contracts

---
+++ src/OfficialAccount/Contracts/Handler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EasyWeChat\OfficialAccount\Contracts;
+
+use EasyWeChat\Kernel\Decorators\FinallyResult;
+use EasyWeChat\Kernel\Decorators\TerminateResult;
+
+interface Handler
+{
+    public function handle(Message $message): bool|FinallyResult|TerminateResult;
+}
--- src/OfficialAccount/Contracts/Server.php
+++ src/OfficialAccount/Contracts/Server.php
@@ -4,4 +4,6 @@ namespace EasyWeChat\OfficialAccount\Contracts;
 
 interface Server
 {
+    public function process(): Response;
+    public function buildResponse($response): array;
 }
