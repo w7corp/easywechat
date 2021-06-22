a423cc06d0c65faa8f014c675cada22d79da19fe
墨娘 <30471441@qq.com>
2021-06-22 18:15:48 +0800

    [work] 修改素材管理接口
    * 增加获取高清语言素材接口
    * 增加上传永久图片接口

--- src/Work/Media/Client.php
+++ src/Work/Media/Client.php
@@ -33,17 +33,7 @@ class Client extends BaseClient
      */
     public function get(string $mediaId)
     {
-        $response = $this->requestRaw('cgi-bin/media/get', 'GET', [
-            'query' => [
-                'media_id' => $mediaId,
-            ],
-        ]);
-
-        if (false !== stripos($response->getHeaderLine('Content-Type'), 'text/plain')) {
-            return $this->castResponseToType($response, $this->app['config']->get('response_type'));
-        }
-
-        return StreamResponse::buildFromPsrResponse($response);
+        return $this->getResources($mediaId, 'cgi-bin/media/get');
     }
 
     /**
@@ -117,5 +107,67 @@ class Client extends BaseClient
         ];
 
         return $this->httpUpload('cgi-bin/media/upload', $files, $form, compact('type'));
+    }
+
+    /**
+     * Upload permanently valid images
+     *
+     * @see https://work.weixin.qq.com/api/doc/90000/90135/90256
+     * @param string $path
+     * @param array $form
+     *
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function uploadImg(string $path, array $form = [])
+    {
+        $files = [
+            'media' => $path,
+        ];
+
+        return $this->httpUpload('cgi-bin/media/uploadimg', $files, $form);
+    }
+
+
+    /**
+     * Get HD voice material
+     *
+     * @see https://work.weixin.qq.com/api/doc/90000/90135/90255
+     * @param string $mediaId
+     *
+     * @return array|\EasyWeChat\Kernel\Http\Response|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getHdVoice(string $mediaId)
+    {
+        return $this->getResources($mediaId, 'cgi-bin/media/get/jssdk');
+    }
+
+    /**
+     * @param string $mediaId
+     * @param string $uri
+     *
+     * @return array|\EasyWeChat\Kernel\Http\Response|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function getResources(string $mediaId, string $uri)
+    {
+        $response = $this->requestRaw($uri, 'GET', [
+            'query' => [
+                'media_id' => $mediaId,
+            ],
+        ]);
+
+        if (false !== stripos($response->getHeaderLine('Content-Type'), 'text/plain')) {
+            return $this->castResponseToType($response, $this->app['config']->get('response_type'));
+        }
+
+        return StreamResponse::buildFromPsrResponse($response);
     }
 }
