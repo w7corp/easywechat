2135f4774d81cf6eec77a0bc2248043d2902011a
kamly <121368588@qq.com>
2021-06-30 20:48:13 +0800

    fix in_array
    超哥手把手教学，修复支持多个 where 
    
    `$this->openWork->server->push([$this, 'handleOrderEvent'])->where('InfoType', ['open_order', 'change_order', 'pay_for_app_success', 'refund']); `

--- src/Kernel/Clauses/Clause.php
+++ src/Kernel/Clauses/Clause.php
@@ -56,9 +56,18 @@ class Clause
     {
         foreach ($this->clauses['where'] as $item) {
             list($key, $value) = $item;
-            if (isset($payload[$key]) && $payload[$key] !== $value) {
+            if (!isset($payload[$key])) {
+                continue;
+            }
+
+            if (is_array($value) && !in_array($payload[$key], $value)) {
+                return true;
+            }
+
+            if (!is_array($value) && $payload[$key] !== $value) {
                 return true;
             }
         }
+        return false;
     }
 }
