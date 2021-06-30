c78a4c17185a704bf65199dc9794618561be9d0b
刘豪 <liuhao25@foxmail.com>
2021-06-30 23:41:52 +0800

    【feature】企业微信通讯录变更回调事件 (#2141)

--- src/OpenWork/Server.php
+++ src/OpenWork/Server.php
@@ -43,6 +43,42 @@ class Server implements ServerInterface
         return $this;
     }
 
+    // 成员变更通知 + 部门变更通知 + 标签变更通知
+    public function handleContactChanged(callable | string $handler): static
+    {
+        $this->with(
+            function ($message, \Closure $next) use ($handler) {
+                return $message->Event === 'change_contact' ? $handler($message) : $next($message);
+            }
+        );
+
+        return $this;
+    }
+
+    public function handleUserTagUpdated(callable | string $handler): static
+    {
+        $this->with(
+            function ($message, \Closure $next) use ($handler) {
+                return
+                    $message->Event === 'change_contact' && $message->$message->ChangeType === 'update_tag' ? $handler($message) : $next($message);
+            }
+        );
+
+        return $this;
+    }
+
+    public function handleBatchJobCompleted(callable | string $handler): static
+    {
+        $this->with(
+            function ($message, \Closure $next) use ($handler) {
+                return
+                    $message->Event === 'batch_job_result' ? $handler($message) : $next($message);
+            }
+        );
+
+        return $this;
+    }
+
     public function transformResponse(array $response, Message $message): ResponseInterface
     {
         return ServerResponse::success();
