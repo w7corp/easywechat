7e918935017fa3011a4f7bfc678499a3f045395e
overtrue <anzhengchao@gmail.com>
2021-06-30 21:30:48 +0800

    Fix http client.

--- src/Kernel/Traits/AccessTokenAwareHttpClient.php
+++ src/Kernel/Traits/AccessTokenAwareHttpClient.php
@@ -7,45 +7,38 @@ namespace EasyWeChat\Kernel\Traits;
 use EasyWeChat\Kernel\Support\UserAgent;
 use EasyWeChat\Kernel\Contracts\AccessToken as AccessTokenInterface;
 use Symfony\Component\HttpClient\HttpClient as SymfonyHttpClient;
-use Symfony\Component\HttpClient\HttpClientTrait;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 use Symfony\Contracts\HttpClient\ResponseStreamInterface;
 
 trait AccessTokenAwareHttpClient
 {
-    use HttpClientTrait;
-
     protected HttpClientInterface $client;
     protected ?AccessTokenInterface $accessToken = null;
 
     public function __construct(
         ?HttpClientInterface $client = null,
         ?AccessTokenInterface $accessToken = null,
-        array $defaultOptions = []
     ) {
         $this->accessToken = $accessToken;
         $this->client = $client ?? SymfonyHttpClient::create();
-        $this->defaultOptions = $defaultOptions;
+    }
 
-        $options = \array_merge(
-            self::OPTIONS_DEFAULTS,
-            [
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                    'Accept' => 'application/json',
-                ],
-            ]
-        );
+    public function withOptions(array $options): self
+    {
+        $clone = clone $this;
+        $clone->defaultOptions = \array_merge($this->defaultOptions, $options);
 
-        [, $this->defaultOptions] = self::prepareRequest(null, null, $options, $this->defaultOptions);
+        return $clone;
     }
 
     public function withAccessToken(AccessTokenInterface $accessToken): static
     {
-        $this->accessToken = $accessToken;
+        $clone = clone $this;
 
-        return $this;
+        $clone->accessToken = $accessToken;
+
+        return $clone;
     }
 
     /**
@@ -53,7 +46,7 @@ trait AccessTokenAwareHttpClient
      */
     public function request(string $method, string $url, array $options = []): ResponseInterface
     {
-        [, $options] = self::prepareRequest($method, $url, $options, $this->defaultOptions);
+        $options = \array_merge($this->defaultOptions, $options);
 
         $options['headers']['User-Agent'] = UserAgent::create([$options['headers']['User-Agent'] ?? '']);
 
