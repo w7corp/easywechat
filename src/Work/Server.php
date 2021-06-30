e64c2e912c07417a1f14967a4b380b452fe8b697
finecho <liuhao25@foxmail.com>
2021-06-30 23:55:05 +0800

    【feature】open-work 事件回调

--- src/Work/Server.php
+++ src/Work/Server.php
@@ -25,4 +25,40 @@ class Server implements ServerInterface
         protected Encryptor $encryptor,
     ) {
     }
+
+    // 成员变更通知 + 部门变更通知 + 标签变更通知
+    public function handleContactChanged(callable | string $handler): static
+    {
+        $this->with(
+            function ($message, \Closure $next) use ($handler) {
+                return $message->Event === 'change_contact' ? $handler($message) : $next($message);
+            }
+        );
+
+        return $this;
+    }
+
+    public function handleUserTagUpdated(callable | string $handler): static
+    {
+        $this->with(
+            function ($message, \Closure $next) use ($handler) {
+                return
+                    $message->Event === 'change_contact' && $message->ChangeType === 'update_tag' ? $handler($message) : $next($message);
+            }
+        );
+
+        return $this;
+    }
+
+    public function handleBatchJobCompleted(callable | string $handler): static
+    {
+        $this->with(
+            function ($message, \Closure $next) use ($handler) {
+                return
+                    $message->Event === 'batch_job_result' ? $handler($message) : $next($message);
+            }
+        );
+
+        return $this;
+    }
 }
