0089314e37ff10baed029a71cab290c6d0b3b5a7
读心印 <839024615@qq.com>
2021-07-02 22:43:22 +0800

    [tests] work

---
+++ tests/Work/AccessTokenTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EasyWeChat\Tests\Work;
+
+use EasyWeChat\Work\AccessToken;
+use PHPUnit\Framework\TestCase;
+use Psr\SimpleCache\CacheInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class AccessTokenTest extends TestCase
+{
+    public function test_get_token_from_http_request()
+    {
+        $httpClient = \Mockery::mock(HttpClientInterface::class);
+        $response = \Mockery::mock(ResponseInterface::class);
+
+        $result = [
+            'access_token' => 'mock_access_token',
+            'expires_in' => '1500',
+        ];
+
+        $response->allows()->toArray()->andReturn($result);
+
+        $config = [
+            'corp_id' => 'wx3cf0f39249000060',
+            'secret' => 'mock-secret',
+        ];
+
+        $options = [
+            'query' => [
+                'corpid' => $config['corp_id'],
+                'corpsecret' => $config['secret'],
+            ],
+        ];
+
+        $httpClient->allows()->request('GET', '/cgi-bin/gettoken', $options)->andReturn($response);
+
+        $accessToken = new AccessToken($config['corp_id'], $config['secret'], null, null, $httpClient);
+
+        $this->assertSame($result['access_token'], $accessToken->getToken());
+    }
+
+    public function test_get_token_from_cache()
+    {
+        $cache = \Mockery::mock(CacheInterface::class);
+
+        $key = 'mock-cache-key';
+
+        $result = [
+            'access_token' => 'mock_access_token',
+            'expires_in' => '1500',
+        ];
+
+        $cache->expects()->get($key)->andReturn($result['access_token']);
+
+        $config = [
+            'corp_id' => 'wx3cf0f39249000060',
+            'secret' => 'mock-secret',
+        ];
+
+        $accessToken = new AccessToken($config['corp_id'], $config['secret'], $key, $cache);
+
+        $this->assertSame($result['access_token'], $accessToken->getToken());
+    }
+
+    public function test_set_key()
+    {
+        $config = [
+            'corp_id' => 'wx3cf0f39249000060',
+            'secret' => 'mock-secret',
+        ];
+
+        $key = 'mock-cache-key';
+
+        $accessToken = new AccessToken($config['corp_id'], $config['secret'], $key);
+
+        $this->assertSame($key, $accessToken->getKey());
+    }
+}
---
+++ tests/Work/ConfigTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EasyWeChat\Tests\Work;
+
+use EasyWeChat\Kernel\Contracts\Config as ConfigInterface;
+use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
+use EasyWeChat\Work\Application;
+use EasyWeChat\Work\Config;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /**
+     * @throws \EasyWeChat\Kernel\Exceptions\RuntimeException
+     */
+    public function test_application_created_can_get_config()
+    {
+        $app = new Application(
+            [
+                'corp_id' => 'wx3cf0f39249000060',
+                'secret' => 'mock-secret',
+                'token' => 'mock-token',
+            ]
+        );
+
+        $this->assertInstanceOf(ConfigInterface::class, $app->getConfig());
+    }
+
+    /**
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     * @throws \EasyWeChat\Kernel\Exceptions\RuntimeException
+     */
+    public function test_set_config_to_application()
+    {
+        $config = [
+            'corp_id' => 'wx3cf0f39249111111',
+            'secret' => 'mock-account-secret',
+            'token' => 'mock-account-token',
+            'aes_key' => 'mock-account-aes-key',
+        ];
+
+        $config = new Config($config);
+
+        $applicationConfig = [
+            'corp_id' => 'wx3cf0f39249000060',
+            'secret' => 'mock-secret',
+            'token' => 'mock-token',
+        ];
+
+        $app = new Application(
+            [
+                'corp_id' => $applicationConfig['corp_id'],
+                'secret' => $applicationConfig['secret'],
+                'token' => $applicationConfig['token'],
+            ]
+        );
+
+        $this->assertInstanceOf(ConfigInterface::class, $app->getConfig());
+        $this->assertSame($applicationConfig['corp_id'], $app->getConfig()->get('corp_id'));
+        $this->assertSame(false, $app->getConfig()->has('aes_key'));
+
+        $app->setConfig($config);
+
+        $this->assertNotSame($applicationConfig['corp_id'], $app->getConfig()->get('corp_id'));
+        $this->assertSame($config['corp_id'], $app->getConfig()->get('corp_id'));
+        $this->assertSame(true, $app->getConfig()->has('aes_key'));
+    }
+
+    public function test_init_config_can_check_missing_keys()
+    {
+        $config = [
+            'secret' => 'mock-account-secret',
+            'token' => 'mock-account-token',
+            'aes_key' => 'mock-account-aes_key'
+        ];
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(\sprintf('"%s" cannot be empty.', 'corp_id'));
+
+        new Config($config);
+    }
+}
