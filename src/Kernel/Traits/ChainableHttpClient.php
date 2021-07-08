9ffbc9a380db68985b9efc578b866c4a11236b64
overtrue <anzhengchao@gmail.com>
2021-07-08 22:58:55 +0800

    dynamic uri

--- src/Kernel/Traits/ChainableHttpClient.php
+++ src/Kernel/Traits/ChainableHttpClient.php
@@ -2,6 +2,7 @@
 
 namespace EasyWeChat\Kernel\Traits;
 
+use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
 use EasyWeChat\Kernel\Support\Str;
 
 trait ChainableHttpClient
@@ -15,7 +16,7 @@ trait ChainableHttpClient
         if (\str_starts_with($uri, 'http://') || \str_starts_with($uri, 'https://')) {
             $clone->uri = $uri;
         } else {
-            $uri = Str::kebab($uri);
+            $uri = \preg_match('~\$[a-z0-9_]+~i', $uri) ? $uri : Str::kebab($uri);
             $clone->uri = \trim(\sprintf('/%s/%s', \trim($this->uri, '/'), \trim($uri, '/')), '/');
         }
 
@@ -41,8 +42,14 @@ trait ChainableHttpClient
         return \call_user_func_array([$this->client, $name], $arguments);
     }
 
-    protected function callWithShortcuts(string $method, string | array $uri = [], array $options = []): \Symfony\Contracts\HttpClient\ResponseInterface
-    {
+    /**
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     */
+    protected function callWithShortcuts(
+        string $method,
+        string | array $uri = [],
+        array $options = []
+    ): \Symfony\Contracts\HttpClient\ResponseInterface {
         if (\is_string($uri)) {
             $uri = $this->withUri($uri)->getUri();
         } else {
@@ -50,6 +57,30 @@ trait ChainableHttpClient
             $uri = $this->getUri();
         }
 
+        [$uri, $options] = $this->replaceUriVariables($uri, $options);
+
         return $this->request(\strtoupper($method), $uri, $options);
+    }
+
+    public function replaceUriVariables(string $uri, array $options): array
+    {
+        return [
+            \preg_replace_callback(
+                pattern: '~\$(?<name>[a-z0-9_]+)~i',
+                callback: function ($matches) use (&$options) {
+                    if (empty($options[$matches['name']])) {
+                        throw new InvalidArgumentException(\sprintf('Missing url variables "%s".', $matches['name']));
+                    }
+
+                    $value = $options[$matches['name']];
+
+                    unset($options[$matches['name']]);
+
+                    return $value;
+                },
+                subject: $uri
+            ),
+            $options,
+        ];
     }
 }
