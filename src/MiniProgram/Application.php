08e30da8b4b3f411363ece168efabe03029ddb92
24Kdabaiyang <haoliang@outlook.jp>
2021-07-17 20:52:38 +0800

    增加自定义版交易组件开放接口-商家入驻接口 (#2159)
    * 增加自定义版交易组件开放接口-接入前必需接口
    
    * 增加自定义版交易组件开放接口- 商家入驻接口
    
    Co-authored-by: 安正超 <anzhengchao@gmail.com>

--- src/MiniProgram/Application.php
+++ src/MiniProgram/Application.php
@@ -49,7 +49,7 @@ use EasyWeChat\Kernel\ServiceContainer;
  * @property \EasyWeChat\MiniProgram\Shop\Basic\Client          $shop_basic
  * @property \EasyWeChat\MiniProgram\Shop\Account\Client        $shop_account
  * @property \EasyWeChat\MiniProgram\Shop\Spu\Client            $shop_spu
- * @property \EasyWeChat\MiniProgram\Shop\Order\Client          $shop_order
+ * @property \EasyWeChat\MiniProgram\Shop\Order\Client          $shop_order·
  */
 class Application extends ServiceContainer
 {
