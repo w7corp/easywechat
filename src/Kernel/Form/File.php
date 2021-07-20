b94a61b1deeafcde328a726c78bf3f88e865d591
overtrue <anzhengchao@gmail.com>
2021-07-20 21:36:12 +0800

    fromString -> withContents

--- src/Kernel/Form/File.php
+++ src/Kernel/Form/File.php
@@ -6,7 +6,7 @@ use Symfony\Component\Mime\Part\DataPart;
 
 class File extends DataPart
 {
-    public static function fromString(string $contents, string $filename, string $contentType = null, string $encoding = null): DataPart
+    public static function withContents(string $contents, string $filename, string $contentType = null, string $encoding = null): DataPart
     {
         $path = \tempnam(\sys_get_temp_dir(), 'part_');
 
