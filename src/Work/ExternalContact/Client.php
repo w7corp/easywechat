e58db78279cf30cccb1a7c06582454f90b398b8b
dengxit <dengxit@gmail.com>
2021-08-12 16:33:57 +0800

    param use Camel-Case

--- src/Work/ExternalContact/Client.php
+++ src/Work/ExternalContact/Client.php
@@ -338,7 +338,7 @@ class Client extends BaseClient
      * @see https://work.weixin.qq.com/api/doc/90000/90135/92122
      *
      * @param string $chatId
-     * @param int $need_name
+     * @param int $needName
      *
      * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
      *
@@ -346,11 +346,11 @@ class Client extends BaseClient
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
 
-    public function getGroupChat(string $chatId, int $need_name = 0)
+    public function getGroupChat(string $chatId, int $needName = 0)
     {
         $params = [
             'chat_id' => $chatId,
-            'need_name' => $need_name,
+            'need_name' => $needName,
         ];
 
         return $this->httpPostJson('cgi-bin/externalcontact/groupchat/get', $params);
