6c4ba9d64e4d9db60cb7d2a9f0f625296e7e76d2
overtrue <anzhengchao@gmail.com>
2021-09-27 16:02:08 +0800

    Fixed #2218

--- src/Kernel/Log/LogManager.php
+++ src/Kernel/Log/LogManager.php
@@ -27,8 +27,16 @@ use Psr\Log\LoggerInterface;
 
 /**
  * Class LogManager.
- *
- * @author overtrue <i@overtrue.me>
+ * 
+ * @method void emergency($message, array $context = [])
+ * @method void alert($message, array $context = [])
+ * @method void critical($message, array $context = [])
+ * @method void error($message, array $context = [])
+ * @method void warning($message, array $context = [])
+ * @method void notice($message, array $context = [])
+ * @method void info($message, array $context = [])
+ * @method void debug($message, array $context = [])
+ * @method void log($level, $message, array $context = [])
  */
 class LogManager implements LoggerInterface
 {
@@ -442,153 +450,6 @@ class LogManager implements LoggerInterface
         $this->customCreators[$driver] = $callback->bindTo($this, $this);
 
         return $this;
-    }
-
-    /**
-     * System is unusable.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function emergency($message, array $context = [])
-    {
-        return $this->driver()->emergency($message, $context);
-    }
-
-    /**
-     * Action must be taken immediately.
-     *
-     * Example: Entire website down, database unavailable, etc. This should
-     * trigger the SMS alerts and wake you up.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function alert($message, array $context = [])
-    {
-        return $this->driver()->alert($message, $context);
-    }
-
-    /**
-     * Critical conditions.
-     *
-     * Example: Application component unavailable, unexpected exception.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function critical($message, array $context = [])
-    {
-        return $this->driver()->critical($message, $context);
-    }
-
-    /**
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function error($message, array $context = [])
-    {
-        return $this->driver()->error($message, $context);
-    }
-
-    /**
-     * Exceptional occurrences that are not errors.
-     *
-     * Example: Use of deprecated APIs, poor use of an API, undesirable things
-     * that are not necessarily wrong.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function warning($message, array $context = [])
-    {
-        return $this->driver()->warning($message, $context);
-    }
-
-    /**
-     * Normal but significant events.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function notice($message, array $context = [])
-    {
-        return $this->driver()->notice($message, $context);
-    }
-
-    /**
-     * Interesting events.
-     *
-     * Example: User logs in, SQL logs.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function info($message, array $context = [])
-    {
-        return $this->driver()->info($message, $context);
-    }
-
-    /**
-     * Detailed debug information.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function debug($message, array $context = [])
-    {
-        return $this->driver()->debug($message, $context);
-    }
-
-    /**
-     * Logs with an arbitrary level.
-     *
-     * @param mixed  $level
-     * @param string $message
-     * @param array  $context
-     *
-     * @return mixed
-     *
-     * @throws \Exception
-     */
-    public function log($level, $message, array $context = [])
-    {
-        return $this->driver()->log($level, $message, $context);
     }
 
     /**
