fdd77e10e5a3088b457755950422544146474f1d
读心印 <839024615@qq.com>
2021-12-15 17:13:08 +0800

    [desc] miniapp short_link url_link

---
+++ docs/5.x/mini-program/url_link.md
@@ -0,0 +1,11 @@
+# URL Link
+
+> 微信文档：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/url-link/urllink.generate.html
+
+> tips: 目前仅针对国内非个人主体的小程序开放.
+
+## 获取小程序 URL Link
+
+```php
+$app->url_link->generate(array $params);
+```
--- docs/5.x/sidebar.js
+++ docs/5.x/sidebar.js
@@ -79,7 +79,9 @@ module.exports = [{
                 { text: "订阅消息", link: "/5.x/mini-program/subscribe_message.html" },
                 { text: "直播", link: "/5.x/mini-program/live.html" },
                 { text: "安全风控", link: "/5.x/mini-program/safety_control.html" },
-                { text: "URL Scheme", link: "/5.x/mini-program/url_scheme_generate.html" },
+                { text: "URL Scheme", link: "/5.x/mini-program/url_scheme.html" },
+                { text: "URL Link", link: "/5.x/mini-program/url_link.html" },
+                { text: "Short Link", link: "/5.x/mini-program/short_link.html" },
             ],
         },
         {
