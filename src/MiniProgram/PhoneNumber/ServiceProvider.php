7d44cdf62aea34ed0dc82cce64dd217dc1b41246
读心印 <33275679+aa24615@users.noreply.github.com>
2021-12-17 00:00:03 +0800

    [feature] miniapp 获取手机号 (#2303)

---
+++ src/MiniProgram/PhoneNumber/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyWeChat\MiniProgram\PhoneNumber;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}.
+     */
+    public function register(Container $app)
+    {
+        $app['phone_number'] = function ($app) {
+            return new Client($app);
+        };
+    }
+}
