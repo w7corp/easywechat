c254286cdeb4b00c149dcd7eb0fa828db8236c87
jiangslee <jiangsili@qq.com>
2021-12-29 15:45:34 +0800

    Update VerifyTicket.php

--- src/OpenPlatform/Contracts/VerifyTicket.php
+++ src/OpenPlatform/Contracts/VerifyTicket.php
@@ -6,8 +6,5 @@ namespace EasyWeChat\OpenPlatform\Contracts;
 
 interface VerifyTicket
 {
-    public function getKey(): string;
-    public function setKey(string $key): static;
     public function getTicket(): string;
-    public function setTicket(string $ticket): static;
 }
