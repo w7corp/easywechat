2edd14c29005756cc4984058bbe06e7d581f1a28
ranpro <584636675@qq.com>
2022-01-06 11:29:13 +0800

    调整内容安全 API 兼容性
    - 支持 1.0 和 2.0 版本

--- src/BasicService/ContentSecurity/Client.php
+++ src/BasicService/ContentSecurity/Client.php
@@ -25,17 +25,15 @@ class Client extends BaseClient
      * Text content security check.
      *
      * @param string $text
-     *
+     * @param array $extra
      * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
      *
      * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function checkText(string $text)
+    public function checkText(string $text, array $extra = [])
     {
-        $params = [
-            'content' => $text,
-        ];
+        $params = array_merge(['content' => $text], $extra);
 
         return $this->httpPostJson('wxa/msg_sec_check', $params);
     }
