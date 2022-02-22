9d0b2ff9f0b9fef7381175e6c0de68676722ea68
沐沫 <lqllife@hotmail.com>
2022-02-22 17:15:10 +0800

    add get open api  quota method (#2371)
    * 增加“查询openAPI调用quota”接口
    
    * 删除“查询openAPI调用quota”中app_id参数

--- src/OfficialAccount/Base/Client.php
+++ src/OfficialAccount/Base/Client.php
@@ -22,6 +22,25 @@ use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
 class Client extends BaseClient
 {
     /**
+     * Get api quota.
+     *
+     * @param  string  $cgiPath  api cgi_path
+     *
+     * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
+     *
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getQuota(string $cgiPath)
+    {
+        $params = [
+            'cgi_path' => $cgiPath,
+        ];
+        
+        return $this->httpPostJson('cgi-bin/openapi/quota/get', $params);
+    }
+    
+    /**
      * Clear quota.
      *
      * @return \Psr\Http\Message\ResponseInterface|\EasyWeChat\Kernel\Support\Collection|array|object|string
