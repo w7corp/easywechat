3d4e73e84ade65ac57c9cb56ca8107b8505bd985
读心印 <839024615@qq.com>
2022-03-03 22:50:49 +0800

    ResponseValidator

--- src/Pay/ResponseValidator.php
+++ src/Pay/ResponseValidator.php
@@ -15,7 +15,15 @@ class ResponseValidator implements \EasyWeChat\Pay\Contracts\ResponseValidator
 
     public function validate(ResponseInterface $response): bool
     {
-        //todo
-        return true;
+        $timestamp = $response->getHeader('Wechatpay-Timestamp')[0] ?? '';
+        $nonce = $response->getHeader('Wechatpay-Nonce')[0] ?? '';
+        $signature = $response->getHeader('Wechatpay-Signature')[0] ?? '';
+        $body = (string)$response->getBody();
+
+        $message = $timestamp . "\n" . $nonce . "\n" . $body . "\n";
+
+        $result = openssl_verify($message, base64_decode($signature), $publicKey, OPENSSL_ALGO_SHA256);
+
+        return $result===1;
     }
 }
