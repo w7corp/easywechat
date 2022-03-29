2f456bddbe48d7327c39fa8c8108ea003f030048
24Kdabaiyang <haoliang@outlook.jp>
2022-03-29 14:12:14 +0800

    自定义交易组件订单接口更新 (#2422)

--- src/MiniProgram/Shop/Order/Client.php
+++ src/MiniProgram/Shop/Order/Client.php
@@ -33,6 +33,19 @@ class Client extends BaseClient
      * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
+    public function create(array $order)
+    {
+        return $this->httpPostJson('shop/order/add', $order);
+    }
+
+    /**
+     * 生成订单
+     *
+     * @param array $order
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function add(array $order)
     {
         return $this->httpPostJson('shop/order/add', $order);
@@ -50,6 +63,19 @@ class Client extends BaseClient
     public function get(string $openid, array $orderId)
     {
         return $this->httpPostJson('shop/order/get', array_merge($orderId, ['openid' => $openid]));
+    }
+
+    /**
+     * 关闭订单
+     *
+     * @param array $params 请求参数
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function close(array $params)
+    {
+        return $this->httpPostJson('shop/order/close', $params);
     }
 
     /**
@@ -75,5 +101,57 @@ class Client extends BaseClient
     public function pay(array $pay)
     {
         return $this->httpPostJson('shop/order/pay', $pay);
+    }
+
+    /**
+     * 同步订单支付结果
+     *
+     * @param array $pay
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function syncPayState(array $pay)
+    {
+        return $this->httpPostJson('shop/order/pay', $pay);
+    }
+
+    /**
+     * 同步订单支付结果
+     *
+     * @param array $params
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getPayInfo(array $params)
+    {
+        return $this->httpPostJson('shop/order/getpaymentparams', $params);
+    }
+
+    /**
+     * 获取推广员订单
+     *
+     * @param array $params
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getFinderOrders(array $params)
+    {
+        return $this->httpPostJson('shop/order/get_list_by_finder', $params);
+    }
+
+    /**
+     * 获取分享员订单
+     *
+     * @param array $params
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getSharerOrders(array $params)
+    {
+        return $this->httpPostJson('shop/order/get_list_by_sharer', $params);
     }
 }
--- tests/MiniProgram/Shop/Order/ClientTest.php
+++ tests/MiniProgram/Shop/Order/ClientTest.php
@@ -43,7 +43,25 @@ class ClientTest extends TestCase
             'out_order_id' => $outOrderId
         ])->andReturn('mock-result');
 
-        $this->assertSame('mock-result', $client->add([
+        $this->assertSame('mock-result', $client->create([
+            'out_order_id' => $outOrderId
+        ]));
+    }
+
+    /**
+     * 生成订单
+     */
+    public function testCreate()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $outOrderId = '456abc';
+
+        $client->expects()->httpPostJson('shop/order/add', [
+            'out_order_id' => $outOrderId
+        ])->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->create([
             'out_order_id' => $outOrderId
         ]));
     }
@@ -77,8 +95,8 @@ class ClientTest extends TestCase
         $client = $this->mockApiClient(Client::class);
 
         $data = [
-            'page'       => 1,
-            'page_size'  => 100,
+            'page' => 1,
+            'page_size' => 100,
             'sort_order' => 1
         ];
 
@@ -100,8 +118,82 @@ class ClientTest extends TestCase
             'out_order_id' => $outOrderId,
         ])->andReturn('mock-result');
 
-        $this->assertSame('mock-result', $client->pay([
+        $this->assertSame('mock-result', $client->syncPayState([
             'out_order_id' => $outOrderId
         ]));
+    }
+
+    /**
+     * 同步订单支付结果
+     */
+    public function testSyncPayState()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $outOrderId = '456abc';
+
+        $client->expects()->httpPostJson('shop/order/pay', [
+            'out_order_id' => $outOrderId,
+        ])->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->syncPayState([
+            'out_order_id' => $outOrderId
+        ]));
+    }
+
+    /**
+     * 获取订单支付信息
+     */
+    public function testGetPayInfo()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $params = [
+            'order_id' => '456abc',
+            'out_order_id' => '20220110220332',
+            'openid' => 'abcdefghijklmn123',
+
+        ];
+
+        $client->expects()->httpPostJson('shop/order/getpaymentparams', $params)->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->getPayInfo($params));
+    }
+
+    /**
+     * 获取推广员订单
+     */
+    public function testGetFinderOrders()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $params = [
+            'page' => 1,
+            'page_size' => 30,
+
+        ];
+
+        $client->expects()->httpPostJson('shop/order/get_list_by_finder', $params)->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->getFinderOrders($params));
+    }
+
+    /**
+     * 获取分享员订单
+     */
+    public function testGetSharerOrders()
+    {
+        $client = $this->mockApiClient(Client::class);
+
+        $params = [
+            'sharer_openid' => '456abc',
+            'page' => 1,
+            'page_size' => 30,
+
+        ];
+
+        $client->expects()->httpPostJson('shop/order/get_list_by_sharer', $params)->andReturn('mock-result');
+
+        $this->assertSame('mock-result', $client->getSharerOrders($params));
     }
 }
