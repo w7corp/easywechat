3f2219535a3a87dfdcf981d5768891c348055f86
overtrue <anzhengchao@gmail.com>
2022-03-30 22:29:41 +0800

    添加提示

--- docs/.vitepress/theme/components/VersionTag.vue
+++ docs/.vitepress/theme/components/VersionTag.vue
@@ -1,3 +1,7 @@
 <template>
-  <sup class="bg-green-500 text-xs text-white px-2 py-1 rounded-lg align-top rounded-bl-none"><slot/></sup>
+  <sup
+    class="bg-green-500 text-xs text-white px-2 py-1 rounded-lg align-top rounded-bl-none"
+    title="该特性需要更新到此版本可用"
+    ><slot
+  /></sup>
 </template>
