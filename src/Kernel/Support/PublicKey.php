af7a023ab0d01cb2389c4e3d727f824f3fbe0e1a
overtrue <anzhengchao@gmail.com>
2026-04-04 08:23:42 +0800

    Harden public key serial parsing

--- src/Kernel/Support/PublicKey.php
+++ src/Kernel/Support/PublicKey.php
@@ -6,6 +6,8 @@ use EasyWeChat\Kernel\Exceptions\InvalidConfigException;
 
 use function file_exists;
 use function file_get_contents;
+use function is_array;
+use function is_string;
 use function openssl_x509_parse;
 use function str_starts_with;
 use function strtoupper;
@@ -24,13 +26,19 @@ class PublicKey
      */
     public function getSerialNo(): string
     {
-        $info = openssl_x509_parse($this->certificate);
+        $info = $this->parseCertificate();
 
         if ($info === false) {
             throw new InvalidConfigException('Read the $certificate failed, please check it whether or nor correct');
         }
 
-        return strtoupper($info['serialNumberHex']);
+        $serialNumber = $info['serialNumberHex'] ?? null;
+
+        if (! is_string($serialNumber) || $serialNumber === '') {
+            throw new InvalidConfigException('Certificate serial number is missing.');
+        }
+
+        return strtoupper($serialNumber);
     }
 
     public function __toString(): string
@@ -40,5 +48,15 @@ class PublicKey
         }
 
         return $this->certificate;
+    }
+
+    /**
+     * @return array<mixed>|false
+     */
+    protected function parseCertificate(): array|false
+    {
+        $info = openssl_x509_parse($this->certificate);
+
+        return is_array($info) ? $info : false;
     }
 }
--- tests/Kernel/Support/PublicKeyTest.php
+++ tests/Kernel/Support/PublicKeyTest.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EasyWeChat\Tests\Kernel\Support;
 
+use EasyWeChat\Kernel\Exceptions\InvalidConfigException;
 use EasyWeChat\Kernel\Support\PublicKey;
 use EasyWeChat\Tests\TestCase;
 
@@ -22,5 +25,59 @@ class PublicKeyTest extends TestCase
         $cert = new PublicKey($path);
 
         $this->assertSame($contents, \strval($cert));
+    }
+
+    public function test_get_serial_no()
+    {
+        $contents = file_get_contents(__DIR__.'/../../fixtures/cert.pem') ?: '';
+        $cert = new PublicKey($contents);
+
+        $this->assertSame('0DC0DF83', $cert->getSerialNo());
+    }
+
+    public function test_get_serial_no_throws_for_invalid_certificate()
+    {
+        $cert = new PublicKey('not-a-cert');
+
+        $this->expectException(InvalidConfigException::class);
+        $this->expectExceptionMessage('Read the $certificate failed, please check it whether or nor correct');
+
+        $cert->getSerialNo();
+    }
+
+    public function test_get_serial_no_throws_when_serial_number_is_missing_without_warnings()
+    {
+        $cert = new MissingSerialNumberPublicKey('mock-certificate');
+        $errors = [];
+
+        set_error_handler(function (int $severity, string $message) use (&$errors): bool {
+            $errors[] = [$severity, $message];
+
+            return true;
+        });
+
+        try {
+            try {
+                $cert->getSerialNo();
+                $this->fail('Expected getSerialNo() to throw.');
+            } catch (InvalidConfigException $e) {
+                $this->assertSame('Certificate serial number is missing.', $e->getMessage());
+            }
+        } finally {
+            restore_error_handler();
+        }
+
+        $this->assertSame([], $errors);
+    }
+}
+
+class MissingSerialNumberPublicKey extends PublicKey
+{
+    /**
+     * @return array<mixed>|false
+     */
+    protected function parseCertificate(): array|false
+    {
+        return ['subject' => ['CN' => 'mock']];
     }
 }
